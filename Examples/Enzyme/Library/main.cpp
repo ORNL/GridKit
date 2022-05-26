88cf75984a3c389988e6e5f8b8a5f6a0bc3481d1
Slaven Peles <peless@ornl.gov>
2022-05-26 12:50:07 -0400

    More verbose tests for enzyme support.

--- Examples/Enzyme/Library/main.cpp
+++ Examples/Enzyme/Library/main.cpp
@@ -1,10 +1,23 @@
+#include <iostream>
 #include "library.hpp"
 
 int __enzyme_autodiff(int (*)(int), ...);
 
 int main() {
   int fail = 0;
-  if (__enzyme_autodiff(square, 5) != 25)
-    fail++;
+  int sq = square(5);
+  int dersq = __enzyme_autodiff(square, 5);
+
+  std::cout << "x = 5, x^2 = " << sq << ", d(x^2)/dx = " << dersq << "\n"; 
+  if (__enzyme_autodiff(square, 5) != 10)
+  {
+    --fail;
+    // std::cout << "Incorrect result\n";
+  }
+  if(fail < 0)
+    std::cout << "Result incorrect\n\n";
+  else
+    std::cout << "Bingo!!\n\n";
+  std:: cout << fail << "\n";
   return fail;
 }
