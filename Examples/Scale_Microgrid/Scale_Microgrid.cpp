c37ca338c6ad6b5684b6fd0d8f1a810229310e6c
Slaven Peles <peless@ornl.gov>
2024-05-31 18:15:59 -0400

    Fix ScaleMicrogrid example: - Remove dependence on data files. - Check whether tests pass. - Fix code style.

--- Examples/Scale_Microgrid/Scale_Microgrid.cpp
+++ Examples/Scale_Microgrid/Scale_Microgrid.cpp
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <filesystem>
 
+#include <Utilities/Testing.hpp>
+#include "SolutionKeys.hpp"
 
 #include <ComponentLib/PowerElectronicsComponents/DistributedGenerator/DistributedGenerator.hpp>
 #include <ComponentLib/PowerElectronicsComponents/MicrogridLine/MicrogridLine.hpp>
@@ -16,11 +18,33 @@
 #include <Solver/Dynamic/Ida.hpp>
 #include <Solver/Dynamic/DynamicSolver.hpp>
 
-//macro to get macro
-#define STRING(x) #x
-#define XSTRING(x) STRING(x)
+
+static int test(size_t Nsize);
 
 int main(int argc, char const *argv[])
+{
+    int retval = 0;
+    retval += test(2);
+    retval += test(4);
+    retval += test(8);
+    if (retval > 0)
+    {
+        std::cout << "Some tests fail!!\n";
+    }
+    else
+    {
+        std::cout << "All tests pass!!\n";
+    }
+    return retval;
+}
+
+/**
+ * @brief Tests network of distributed generators.
+ * 
+ * @param Nsize - The number of DG line load cobinations to generate for scale
+ * @return int returns 0 if successful, >0 otherwise
+ */
+int test(size_t Nsize)
 {
     double abstol = 1.0e-8;
     double reltol = 1.0e-8;
@@ -31,19 +55,31 @@ int main(int argc, char const *argv[])
     //Create circuit model
     ModelLib::PowerElectronicsModel<double, size_t>* sysmodel = new ModelLib::PowerElectronicsModel<double, size_t>(reltol, abstol, usejac, max_step_amount);
 
+    const std::vector<double>* true_vec = &answer_key_N8;
+
+    switch (Nsize)
+    {
+        case 2:
+            true_vec = &answer_key_N2;
+            break;
+        case 4:
+            true_vec = &answer_key_N4;
+            break;
+        case 8:
+            // true_vec = &answer_key_N8;
+            break;
+        default:
+            std::cout << "No reference solution for Nsize = " << Nsize << ".\n";
+            std::cout << "Using default Nsize = 8.\n";
+    }
+
     
-    //The amount of DG line load cobinations to generate for scale
-    size_t Nsize = 8;
-
-    //Set to false if the parameters choosen are not used to generate data files
-    bool orgparams = true;
-
-    //Modeled after the problem in the paper
+    // Modeled after the problem in the paper
     // Every Bus has the same virtual resistance. This is due to the numerical stability as mentioned in the paper.
     double RN = 1.0e4;
 
-    //DG Params Vector
-    //All DGs have the same set of parameters except for the first two.
+    // DG Params Vector
+    // All DGs have the same set of parameters except for the first two.
     ModelLib::DistributedGeneratorParameters<double, size_t> DG_parms1;
     DG_parms1.wb_ = 2.0*M_PI*50.0;
     DG_parms1.wc_ = 31.41;
@@ -127,19 +163,18 @@ int main(int argc, char const *argv[])
 
 
     //Create the reference DG
-    ModelLib::DistributedGenerator<double, size_t> *dg_ref = new ModelLib::DistributedGenerator<double, size_t>(0, DGParams_list[0], true);
+    ModelLib::DistributedGenerator<double, size_t>* dg_ref = new ModelLib::DistributedGenerator<double, size_t>(0, DGParams_list[0], true);
     //ref motor
-    dg_ref->setExternalConnectionNodes(0,vec_size_internals);
+    dg_ref->setExternalConnectionNodes(0, vec_size_internals);
     //outputs
-    dg_ref->setExternalConnectionNodes(1,vdqbus_index[0]);
-    dg_ref->setExternalConnectionNodes(2,vdqbus_index[0] + 1);
+    dg_ref->setExternalConnectionNodes(1, vdqbus_index[0]);
+    dg_ref->setExternalConnectionNodes(2, vdqbus_index[0] + 1);
     //"grounding" of the difference
-    dg_ref->setExternalConnectionNodes(3,-1);
+    dg_ref->setExternalConnectionNodes(3, -1);
     //internal connections
     for (size_t i = 0; i < 12; i++)
     {
-        
-        dg_ref->setExternalConnectionNodes(4 + i,i);
+        dg_ref->setExternalConnectionNodes(4 + i, i);
     }
     sysmodel->addComponent(dg_ref);
 
@@ -149,9 +184,8 @@ int main(int argc, char const *argv[])
     //Add all other DGs
     for (size_t i = 1; i < 2*Nsize; i++)
     {
-        
         //current DG to add
-        ModelLib::DistributedGenerator<double, size_t> *dg = new ModelLib::DistributedGenerator<double, size_t>(model_id++, DGParams_list[i], false);
+        ModelLib::DistributedGenerator<double, size_t>* dg = new ModelLib::DistributedGenerator<double, size_t>(model_id++, DGParams_list[i], false);
         //ref motor
         dg->setExternalConnectionNodes(0,vec_size_internals);
         //outputs
@@ -160,7 +194,6 @@ int main(int argc, char const *argv[])
         //internal connections
         for (size_t j = 0; j < 13; j++)
         {
-            
             dg->setExternalConnectionNodes(3 + j,indexv + j);
         }
         indexv += 13;
@@ -171,19 +204,19 @@ int main(int argc, char const *argv[])
     for (size_t i = 0; i < 2*Nsize - 1; i++)
     {
         //line
-        ModelLib::MicrogridLine<double, size_t> *line_model = new ModelLib::MicrogridLine<double, size_t>(model_id++, rline_list[i], Lline_list[i]);
+        ModelLib::MicrogridLine<double, size_t>* line_model = new ModelLib::MicrogridLine<double, size_t>(model_id++, rline_list[i], Lline_list[i]);
         //ref motor
-        line_model->setExternalConnectionNodes(0,vec_size_internals);
+        line_model->setExternalConnectionNodes(0, vec_size_internals);
         //input connections
-        line_model->setExternalConnectionNodes(1,vdqbus_index[i]);
-        line_model->setExternalConnectionNodes(2,vdqbus_index[i] + 1);
+        line_model->setExternalConnectionNodes(1, vdqbus_index[i]);
+        line_model->setExternalConnectionNodes(2, vdqbus_index[i] + 1);
         //output connections
-        line_model->setExternalConnectionNodes(3,vdqbus_index[i+1]);
-        line_model->setExternalConnectionNodes(4,vdqbus_index[i+1] + 1);
+        line_model->setExternalConnectionNodes(3, vdqbus_index[i+1]);
+        line_model->setExternalConnectionNodes(4, vdqbus_index[i+1] + 1);
         //internal connections
         for (size_t j = 0; j < 2; j++)
         {
-            line_model->setExternalConnectionNodes(5 + j,indexv + j);
+            line_model->setExternalConnectionNodes(5 + j, indexv + j);
         }
         indexv += 2;
         sysmodel->addComponent(line_model);
@@ -192,17 +225,16 @@ int main(int argc, char const *argv[])
     //  Load all the Load components
     for (size_t i = 0; i < Nsize; i++)
     {
-        ModelLib::MicrogridLoad<double, size_t> *load_model = new ModelLib::MicrogridLoad<double, size_t>(model_id++, rload_list[i], Lload_list[i]);
+        ModelLib::MicrogridLoad<double, size_t>* load_model = new ModelLib::MicrogridLoad<double, size_t>(model_id++, rload_list[i], Lload_list[i]);
         //ref motor
-        load_model->setExternalConnectionNodes(0,vec_size_internals);
+        load_model->setExternalConnectionNodes(0, vec_size_internals);
         //input connections
-        load_model->setExternalConnectionNodes(1,vdqbus_index[2*i]);
-        load_model->setExternalConnectionNodes(2,vdqbus_index[2*i] + 1);
+        load_model->setExternalConnectionNodes(1, vdqbus_index[2*i]);
+        load_model->setExternalConnectionNodes(2, vdqbus_index[2*i] + 1);
         //internal connections
         for (size_t j = 0; j < 2; j++)
         {
-            
-            load_model->setExternalConnectionNodes(3 + j,indexv + j);
+            load_model->setExternalConnectionNodes(3 + j, indexv + j);
         }
         indexv += 2;
         sysmodel->addComponent(load_model);
@@ -211,7 +243,7 @@ int main(int argc, char const *argv[])
     //Add all the microgrid Virtual DQ Buses
     for (size_t i = 0; i < 2*Nsize; i++)
     {
-        ModelLib::MicrogridBusDQ<double, size_t> *virDQbus_model = new ModelLib::MicrogridBusDQ<double, size_t>(
+        ModelLib::MicrogridBusDQ<double, size_t>* virDQbus_model = new ModelLib::MicrogridBusDQ<double, size_t>(
         model_id++, RN);
 
         virDQbus_model->setExternalConnectionNodes(0, vdqbus_index[i]);
@@ -280,38 +312,28 @@ int main(int argc, char const *argv[])
         printf("%lu: % 2.16e\n", i, yfinial[i]);
     }
 
-    //if proper MATLAB results are avalible and model is using origional parameters
-    std::vector<size_t> compDataSizes = {2,4,8};
-    //All data generated with abstol=1e-12 and reltol=1e-12 with ode23tb
-    if (orgparams && compDataSizes.end() != std::find(compDataSizes.begin(), compDataSizes.end(), Nsize))
-    {   
-        //get the cmake source dir from the cmake configuration
-        std::string base_path = XSTRING(SOURCE_ROOT);
+    bool test_pass = true;
+    double tol = 3e-3;
 
-        base_path += "/Examples/Scale_Microgrid/microgrid_N";
-        base_path += std::to_string(Nsize);
-        base_path += ".txt";
-        std::cout << base_path << std::endl;
-
-        //load vector from data file
-        std::vector<double> true_vec(vec_size_total, 0.0);
-        double val_cur = 0.0;
-        std::ifstream data_file(base_path);
-        size_t i = 0;
-        while (data_file >> val_cur && i < true_vec.size())
-        {
-            true_vec[i++] = val_cur;
-        }
-        data_file.close();
-
-        //check relative error
-        std::cout << "Test the Relative Error\n";
-        for (size_t i = 0; i < true_vec.size(); i++)
-        {
-            printf("%lu: % 2.16e\n", i, abs(true_vec[i] - yfinial[i]) / abs(true_vec[i]));
-        }
+    // check relative error
+    std::cout << "Test the Relative Error\n";
+    for (size_t i = 0; i < true_vec->size(); i++)
+    {
+        test_pass *= GridKit::Testing::isEqual(yfinial[i], true_vec->at(i), tol);
+        printf("%lu: % 2.16e\n", i, abs(true_vec->at(i) - yfinial[i]) / abs(true_vec->at(i)));
     }
     
+    delete idas;
+    delete sysmodel; 
 
-    return 0;
+    if(test_pass)
+    {
+        std::cout << "Test with Nsize = " << Nsize << " passes!\n";
+        return 0;
+    }
+    else
+    {
+        std::cout << "Test with Nsize = " << Nsize << " fails!\n";
+        return 1;
+    }
 }
