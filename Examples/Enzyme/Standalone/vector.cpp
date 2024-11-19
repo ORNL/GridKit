119b57282d3fb673d561c4c8cfe6e75548bc0e43
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2024-11-19 16:17:46 -0500

    Jacobian of vector input function is diagonal. Using Enzyme in forward mode.

--- Examples/Enzyme/Standalone/vector.cpp
+++ Examples/Enzyme/Standalone/vector.cpp
@@ -2,52 +2,71 @@
 #include <limits>
 #include <vector>
 
+int enzyme_dupnoneed;
+int enzyme_dup;
+int enzyme_const;
+void __enzyme_fwddiff(void*, ...);
+
 inline
-double square(double x) {
+double square_scalar(double x) {
   return x * x;
 }
 
 inline
-double dsquare_ref(double x) {
+double dsquare_ref_scalar(double x) {
   return 2.0 * x;
 }
 
-void square(std::vector<double> x, std::vector<double>& y) {
-  for (int idx = 0; idx < x.size(); ++idx)
+void square(int N, double* x, double* y) {
+  for (int idx = 0; idx < N; ++idx)
   {
-    y[idx] = square(x[idx]);
+    y[idx] = square_scalar(x[idx]);
   }
 }
 
-void dsquare_ref(std::vector<double> x, std::vector<double> y, std::vector<double>& dy) {
-  for (int idy = 0; idy < y.size(); ++idy)
+void dsquare_ref(int N, double* x, double* y, double* dy) {
+  for (int idy = 0; idy < N; ++idy)
   {
-    for (int idx = 0; idx < x.size(); ++idx)
+    for (int idx = 0; idx < N; ++idx)
     {
-      dy[idy*x.size()+idx] = dsquare_ref(x[idx]);
+      dy[idy*N+idx] = 0.0;
+      if (idx == idy) 
+        dy[idy*N+idx] = dsquare_ref_scalar(x[idx]);
     }
   }
 }
 
-double __enzyme_autodiff(double(*)(double), ...);
-double dsquare(double x) {
-  return __enzyme_autodiff(square, x);
-}
-
-void dsquare(std::vector<double> x, std::vector<double> y, std::vector<double>& dy) {
-  for (int idy = 0; idy < y.size(); ++idy)
+void dsquare(int N, double* x, double* y, double* dy) {
+  double* v = new double[N];
+  double* d_y = new double[N];
+  for (int idy = 0; idy < N; ++idy)
   {
-    for (int idx = 0; idx < x.size(); ++idx)
+    // Elementary vector for Jacobian-vector product
+    for (int idx = 0; idx < N; ++idx)
     {
-      dy[idy*x.size()+idx] = dsquare(x[idx]);
+      v[idx] = 0.0;
+    }
+    v[idy] = 1.0;
+
+    // Autodiff
+    __enzyme_fwddiff((void*)square, enzyme_const, N, 
+                                    enzyme_dup, x, v,
+                                    enzyme_dupnoneed, y, d_y);
+
+    // Store result
+    for (int idx = 0; idx < N; ++idx)
+    {
+      dy[idy*N+idx] = d_y[idx];
     }
   }
+  delete[] v;
+  delete[] d_y;
 }
 
 int main()
 {
   // Vector declarations
-  constexpr int N = 10;
+  constexpr int N = 2;
   std::vector<double> x(N);
   std::vector<double> sq(N);
   std::vector<double> dsq(N*N);
@@ -61,13 +80,13 @@ int main()
   }
 
   // Function evaluation
-  square(x, sq);
+  square(x.size(), x.data(), sq.data());
 
   // Reference Jacobian
-  dsquare_ref(x, sq, dsq_ref);
+  dsquare_ref(x.size(), x.data(), sq.data(), dsq_ref.data());
 
   // Enzyme Jacobian
-  dsquare(x, sq, dsq);
+  dsquare(x.size(), x.data(), sq.data(), dsq.data());
 
   // Check
   int fail = 0;
