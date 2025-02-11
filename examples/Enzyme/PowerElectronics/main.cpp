7a42e5f1a7459c375cdae9048707ec6c78275349
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-02-10 21:40:57 -0500

    Working Enzyme derivative of DistributedGenerator with evalResidual copied over. Need to make it work when using member functions.

--- examples/Enzyme/PowerElectronics/main.cpp
+++ examples/Enzyme/PowerElectronics/main.cpp
@@ -12,35 +12,99 @@ using DGParameters = ModelLib::DistributedGeneratorParameters<double, size_t>;
 int enzyme_dupnoneed;
 int enzyme_dup;
 int enzyme_const;
+void __enzyme_fwddiff(void*, ...);
 
-template <typename T>
-void __enzyme_fwddiff(void*, int, T*, double*, int, T*, double*);
+// Copy from DistributedGenerator<ScalarT, IdxT>::evaluateResidual
+// Need to find a way to differentiate the member function directly
+void evaluateResidual(double* y_, double* f_) 
+{
+    constexpr double wb_ = 2.0*M_PI*50.0;
+    constexpr double wc_ = 31.41;
+    constexpr double mp_ = 9.4e-5;
+    constexpr double Vn_ = 380.0;
+    constexpr double nq_ = 1.3e-3;
+    constexpr double F_ = 0.75;
+    constexpr double Kiv_ = 420.0;
+    constexpr double Kpv_ = 0.1;
+    constexpr double Kic_ = 2.0e4;
+    constexpr double Kpc_ = 15.0;
+    constexpr double Cf_ = 5.0e-5;
+    constexpr double rLf_ = 0.1;
+    constexpr double Lf_ = 1.35e-3;
+    constexpr double rLc_ = 0.03;
+    constexpr double Lc_ = 0.35e-3;
 
-template <typename T>
-void wrapper(T* model, double* res) {
-    model->evaluateResidual();
-    res = (model->getResidual()).data();
+    constexpr bool refframe_ = true;
+
+    std::vector<double> yp_(0);
+
+    double omega = wb_ - mp_ * y_[4];
+    if (refframe_)
+    {
+        f_[0] = omega - y_[0];
+    }
+    else
+    {
+        f_[0] = 0.0;
+    }
+    
+    f_[1] = cos(y_[3]) * y_[14] - sin(y_[3]) * y_[15];
+    f_[2] = sin(y_[3]) * y_[14] + cos(y_[3]) * y_[15];
+
+    double vbd_in = cos(y_[3]) * y_[1] + sin(y_[3]) * y_[2];
+    double vbq_in = -sin(y_[3]) * y_[1] + cos(y_[3]) * y_[2];
+
+    f_[3] = -yp_[3] + omega - y_[0];
+    f_[4] = -yp_[4] + wc_ * (y_[12] * y_[14] + y_[13] * y_[15] - y_[4]);
+    f_[5] = -yp_[5] + wc_ * (-y_[12] * y_[15] + y_[13] * y_[14] - y_[5]);
+
+    double vod_star = Vn_ - nq_ * y_[5];
+    double voq_star = 0.0;
+
+    f_[6] = -yp_[6] + vod_star - y_[12];
+    f_[7] = -yp_[7] + voq_star - y_[13];
+
+    double ild_star = F_ * y_[14] - wb_ * Cf_ * y_[13] + Kpv_ * (vod_star - y_[12]) + Kiv_ * y_[6];
+    double ilq_star = F_ * y_[15] + wb_ * Cf_ * y_[12] + Kpv_ * (voq_star - y_[13]) + Kiv_ * y_[7];
+
+    f_[8] = -yp_[8] + ild_star - y_[10];
+    f_[9] = -yp_[9] + ilq_star - y_[11];
+
+    double vid_star = -wb_ * Lf_ * y_[11] + Kpc_ * (ild_star - y_[10]) + Kic_ * y_[8];
+    double viq_star = wb_ * Lf_ * y_[10] + Kpc_ * (ilq_star - y_[11]) + Kic_ * y_[9]; 
+    
+    f_[10] = -yp_[10] - (rLf_ / Lf_) * y_[10] + omega * y_[11] +  (vid_star - y_[12]) / Lf_;
+    f_[11] = -yp_[11] - (rLf_ / Lf_) * y_[11] - omega * y_[10] +  (viq_star - y_[13]) / Lf_;
+
+    f_[12] = -yp_[12] + omega * y_[13] + (y_[10] - y_[14]) / Cf_;
+    f_[13] = -yp_[13] - omega * y_[12] + (y_[11] - y_[15]) / Cf_;
+
+    f_[14] = -yp_[14] - (rLc_ / Lc_) * y_[14] + omega * y_[15] + (y_[12] - vbd_in) / Lc_;
+    f_[15] = -yp_[15] - (rLc_ / Lc_) * y_[15] - omega * y_[14] + (y_[13] - vbq_in) / Lc_;
 }
 
 template <typename T>
 void EnzymeModelJacobian(T* model, DenseMatrix& jac) {
     int N = model->size();
-    T d_model(*model);
+    double* y = new double[N];
     double* v = new double[N];
+    double* res = new double[N];
     double* d_res = new double[N];
     for (int idy = 0; idy < N; ++idy)
     {
         // Elementary vector for Jacobian-vector product
         for (int idx = 0; idx < N; ++idx)
         {
+            y[idx] = (model->y())[idx];
+            res[idx] = (model->getResidual())[idx];
             v[idx] = 0.0;
         }
         v[idy] = 1.0;
   
-        //// Autodiff
-        //__enzyme_fwddiff<T>((void*)wrapper<T>, 
-        //                    enzyme_dup, model, v,
-        //                    enzyme_dupnoneed, &d_model, d_res);
+        // Autodiff
+        __enzyme_fwddiff((void*)evaluateResidual, 
+                         enzyme_dup, y, v,
+                         enzyme_dupnoneed, res, d_res);
   
         // Store result
         for (int idx = 0; idx < N; ++idx)
@@ -50,7 +114,9 @@ void EnzymeModelJacobian(T* model, DenseMatrix& jac) {
         }
     }
 
+    delete[] y;
     delete[] v;
+    delete[] res;
     delete[] d_res;
 }
 
@@ -95,14 +161,13 @@ int main() {
     bool verbose = true;
     if (verbose)
     {
-        for (int idx = 0; idx < dg->size(); ++idx)
-        {
-            std::cout << "i = " << idx << ", y = " << y[idx] << ", yp = " << yp[idx] << ", res = " << res[idx] <<"\n";
-        }
-        std::cout << "Autodiff Jacobian\n"; 
-        jac_COO->printMatrix();
-        std::cout << "Reference Jacobian\n"; 
-        jac_ref.printMatrix();
+        //for (int idx = 0; idx < dg->size(); ++idx)
+        //{
+        //    std::cout << "i = " << idx << ", y = " << y[idx] << ", yp = " << yp[idx] << ", res = " << res[idx] <<"\n";
+        //}
+        jac_autodiff.printMatrix("Autodiff Jacobian");
+        jac_COO->printMatrix("Autodiff Jacobian");
+        jac_ref.printMatrix("Reference Jacobian");
     }
     std::cout << "Status: " << fail << "\n";
 
