c3d86e6a2250710bba6fdf6e112819228a5996d1
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2025-02-27 11:17:40 -0500

    Additional Enzyme examples and use cases (e.g DistributedGenerator) (#54)
    * Add preliminary Enzyme support
    
    * Use better naming conventions
    
    * More verbose tests for enzyme support.
    
    * int --> double in Enzyme examples.
    
    * Separate scalar and vector examples using Enzyme.
    
    * Use classes in EnzymeLibCheck.
    
    * Basic usage of DenseMatrix in EnzymeVector example.
    
    * Method to convert Dense matriz to COO.
    
    * Use DenseMatrix to store the Jacobians in Enzyme vector example.
    
    * Working Enzyme derivative of DistributedGenerator
    
    * EnzymeLibVectorCheck.
    
    * Better documentation of the DenseMatrix class.
    
    ---------
    
    Co-authored-by: Asher Mancinelli <ashermancinelli@gmail.com>
    Co-authored-by: pelesh <peles2@llnl.gov>
    Co-authored-by: Slaven Peles <peless@ornl.gov>

---
+++ examples/LinearAlgebra/DenseTest/DenseTest.cpp
@@ -0,0 +1,26 @@
+
+#include <LinearAlgebra/DenseMatrix/DenseMatrix.hpp>
+
+int main()
+{
+    int fail = 0;
+
+    size_t m = 4;
+    size_t n = 4;
+    GridKit::LinearAlgebra::DenseMatrix<double, size_t> A = 
+      GridKit::LinearAlgebra::DenseMatrix<double, size_t>(m, n);
+
+    double val = 0.0;
+    for (size_t j = 0; j < n; ++j)
+    {
+        for (size_t i = 0; i < m; ++i)
+        {
+            A.setValue(i, j, val);
+            val += 1.0;
+        }
+    }
+    A.printMatrix("Dense matrix test output");
+    (A.getValuesCOO())->printMatrix();
+
+    return fail;
+}
