d120d24e4cb8a8ea3dc02a6a0f03772998bce06c
abirchfield <adam.b.birchfield@gmail.com>
2025-04-11 10:19:40 -0500

    Unit test for gen residual

--- tests/UnitTests/PhasorDynamics/GenrouTests.hpp
+++ tests/UnitTests/PhasorDynamics/GenrouTests.hpp
@@ -46,6 +46,23 @@ namespace GridKit
       {
         TestStatus success = true;
         success.skipTest();
+        PhasorDynamics::Bus<ScalarT, IdxT> bus(1.0, 0.0);
+        PhasorDynamics::Genrou<ScalarT, IdxT> gen(&bus, 1, 1, 0.05013, 3, 0,
+          0, 7, 0.04, 0.05, 0.75, 2.1, 0.2, 0.18, 0.5, 0.5, 0.18, 0.15, 0, 0);
+
+        bus.allocate();
+        bus.initialize();
+        bus.evaluateResidual();
+
+        gen.allocate();
+        gen.initialize();
+        gen.evaluateResidual();
+        
+        const std::vector<ScalarT>& f = gen.getResidual();
+        for(int i = 0; i < 21; ++i) 
+        {
+          if (fabs(f[i]) > 1e-10) success = false;
+        }
 
         return success.report(__func__);
       }
