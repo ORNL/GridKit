5d78001653ec3507e39bb99cecdb3ca9a27ef39f
abdourahmanbarry <abdourahman@vt.edu>
2025-04-13 19:10:42 -0400

    separate declaration and implementation of ClassicalGen  into header and source file

--- src/Model/PhasorDynamics/SynchronousMachine/ClassicalGenerator/ClassicalGen.cpp
+++ src/Model/PhasorDynamics/SynchronousMachine/ClassicalGenerator/ClassicalGen.cpp
@@ -1,1 +1,251 @@
-#include "GEN2.hpp"
+/**
+ * @file Genrou.cpp
+ * @author Adam Birchfield (abirchfield@tamu.edu)
+ * @author Slaven Peles (peless@ornl.gov)
+ * @brief Definition of a GENROU generator model.
+ *
+ *
+ */
+
+ #include "ClassicalGen.hpp"
+
+ #include <cmath>
+ #include <iostream>
+ 
+ #include <Model/PhasorDynamics/Bus/Bus.hpp>
+ 
+ #define _USE_MATH_DEFINES
+ 
+ namespace GridKit
+ {
+   namespace PhasorDynamics
+   {
+     /*!
+      * @brief Constructor for a pi-model branch
+      *
+      * Arguments passed to ModelEvaluatorImpl:
+      * - Number of equations = 0
+      * - Number of independent variables = 0
+      * - Number of quadratures = 0
+      * - Number of optimization parameters = 0
+      */
+     template <class ScalarT, typename IdxT>
+     ClassicalGen<ScalarT, IdxT>::ClassicalGen(bus_type* bus, int unit_id)
+       : bus_(bus),
+         busID_(0),
+         unit_id_(unit_id),
+         H_(3.),
+         D_(0.),
+         Ra_(0.),
+         Xdp_(.5),
+         pmech_(0.2),
+         ep_(0.2)
+     {
+       size_ = 5;
+       setDerivedParams();
+ 
+       // Temporary, to eliminate compiler warnings
+       (void) busID_;
+       (void) unit_id_;
+     }
+ 
+     /*!
+      * @brief Constructor for a pi-model branch
+      *
+      * Arguments passed to ModelEvaluatorImpl:
+      * - Number of equations = 0
+      * - Number of independent variables = 0
+      * - Number of quadratures = 0
+      * - Number of optimization parameters = 0
+      */
+     template <class ScalarT, typename IdxT>
+     ClassicalGen<ScalarT, IdxT>::ClassicalGen(bus_type* bus,
+                                   int       unit_id,
+                                   real_type H,
+                                   real_type D,
+                                   real_type Ra,
+                                   real_type Xdp,
+                                   real_type pmech,
+                                   real_type ep)
+                                   
+       : bus_(bus),
+         busID_(0),
+         unit_id_(unit_id),
+         H_(H),
+         D_(D),
+         Ra_(Ra),
+         Xdp_(Xdp),
+         pmech_(pmech),
+         ep_(ep)
+     {
+       size_ = 5;
+       setDerivedParams();
+     }
+ 
+     // /**
+     //  * @brief Destroy the Genrou
+     //  *
+     //  * @tparam ScalarT
+     //  * @tparam IdxT
+     //  */
+     // template <class ScalarT, typename IdxT>
+     // Genrou<ScalarT, IdxT>::~Genrou()
+     // {
+     //   // std::cout << "Destroy Genrou..." << std::endl;
+     // }
+ 
+     /*!
+      * @brief allocate method computes sparsity pattern of the Jacobian.
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::allocate()
+     {
+       f_.resize(size_);
+       y_.resize(size_);
+       yp_.resize(size_);
+       tag_.resize(size_);
+       fB_.resize(size_);
+       yB_.resize(size_);
+       ypB_.resize(size_);
+       return 0;
+     }
+ 
+     /**
+      * Initialization of the branch model
+      *
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::initialize()
+     {
+       
+       return 0;
+     }
+ 
+     /**
+      * \brief Identify differential variables.
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::tagDifferentiable()
+     {
+
+       return 0;
+     }
+ 
+     /**
+      * \brief Residual contribution of the branch is pushed to the
+      * two terminal buses.
+      *
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::evaluateResidual()
+     {
+       /* Read variables */
+       ScalarT delta  = y_[0];
+       ScalarT omega  = y_[1];
+       ScalarT telec  = y_[2];
+       ScalarT ir     = y_[3];
+       ScalarT ii     = y_[4];
+
+       /* Read derivatives */
+       ScalarT delta_dot = yp_[0];
+       ScalarT omega_dot = yp_[1];
+
+       /* 6 ClassicalGen differential equations */
+       f_[0] = delta_dot - omega * (2 * M_PI * 60);
+       f_[1] = omega_dot - (1.0 / (2 * H_)) * ((pmech_ - D_ * omega) / (1 + omega) - telec);
+       
+       /* 11 ClassicalGen algebraic equations */
+       f_[2] = telec - (1.0/(1.0 + omega))*(g*ep_*ep_ - ep_*(cos(delta)*(g*Vr() - b*Vi()) + sin(delta)*(b*Vr() + g*Vi())));
+
+       f_[3] = ir + g*Vr() - b * Vi()  - ep_*(g*cos(delta) -b*sin(delta));
+       f_[4] = ii + b*Vr() +  g * Vi() - ep_*(b*cos(delta) + g*sin(delta));
+
+       Ir() += - (g*Vr() - b * Vi()  - ep_*(g*cos(delta) -b*sin(delta)));
+       Ii() += - (b*Vr() +  g * Vi() - ep_*(b*cos(delta) + g*sin(delta)));
+
+       return 0;
+     }
+ 
+     /**
+      * @brief Jacobian evaluation not implemented yet
+      *
+      * @tparam ScalarT - scalar data type
+      * @tparam IdxT    - matrix index data type
+      * @return int - error code, 0 = success
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::evaluateJacobian()
+     {
+       return 0;
+     }
+ 
+     /**
+      * @brief Integrand (objective) evaluation not implemented yet
+      *
+      * @tparam ScalarT - scalar data type
+      * @tparam IdxT    - matrix index data type
+      * @return int - error code, 0 = success
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::evaluateIntegrand()
+     {
+       // std::cout << "Evaluate Integrand for Genrou..." << std::endl;
+       return 0;
+     }
+ 
+     /**
+      * @brief Adjoint initialization not implemented yet
+      *
+      * @tparam ScalarT - scalar data type
+      * @tparam IdxT    - matrix index data type
+      * @return int - error code, 0 = success
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::initializeAdjoint()
+     {
+       // std::cout << "Initialize adjoint for Genrou..." << std::endl;
+       return 0;
+     }
+ 
+     /**
+      * @brief Adjoint residual evaluation not implemented yet
+      *
+      * @tparam ScalarT - scalar data type
+      * @tparam IdxT    - matrix index data type
+      * @return int - error code, 0 = success
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::evaluateAdjointResidual()
+     {
+       // std::cout << "Evaluate adjoint residual for Genrou..." << std::endl;
+       return 0;
+     }
+ 
+     /**
+      * @brief Adjoint integrand (objective) evaluation not implemented yet
+      *
+      * @tparam ScalarT - scalar data type
+      * @tparam IdxT    - matrix index data type
+      * @return int - error code, 0 = success
+      */
+     template <class ScalarT, typename IdxT>
+     int ClassicalGen<ScalarT, IdxT>::evaluateAdjointIntegrand()
+     {
+       // std::cout << "Evaluate adjoint Integrand for Genrou..." << std::endl;
+       return 0;
+     }
+ 
+     template <class ScalarT, typename IdxT>
+     void ClassicalGen<ScalarT, IdxT>::setDerivedParams()
+     {
+       g   = Ra_ / (Ra_ * Ra_ + Xdp_ * Xdp_);
+       b   = Xdp_ / (Ra_ * Ra_ + Xdp_ * Xdp_);
+     }
+ 
+     // Available template instantiations
+     template class ClassicalGen<double, long int>;
+     template class ClassicalGen<double, size_t>;
+ 
+   } // namespace PhasorDynamics
+ } // namespace GridKit
+ 
--- src/Model/PhasorDynamics/SynchronousMachine/ClassicalGenerator/ClassicalGen.hpp
+++ src/Model/PhasorDynamics/SynchronousMachine/ClassicalGenerator/ClassicalGen.hpp
@@ -1,213 +1,123 @@
-/* GENROU Component - Adam Birchfield */
-#pragma once
+/**
+ * @file Genrou.cpp
+ * @author Adam Birchfield (abirchfield@tamu.edu)
+ * @author Slaven Peles (peless@ornl.gov)
+ * @brief Declaration of a GENROU generator model.
+ *
+ */
 
-#define _USE_MATH_DEFINES
-#include <Model/PhasorDynamics/BusBase.hpp>
-#include <Model/PhasorDynamics/Component.hpp>
+ #pragma once
 
-namespace GridKit
-{
-  namespace PhasorDynamics
-  {
-    using ComponentT = Component<double, size_t>;
-    using BaseBusT   = BusBase<double, size_t>;
-
-    class ClassicalGen : public ComponentT
-    {
-      using ComponentT::alpha_;
-      using ComponentT::f_;
-      using ComponentT::fB_;
-      using ComponentT::g_;
-      using ComponentT::gB_;
-      using ComponentT::nnz_;
-      using ComponentT::param_;
-      using ComponentT::size_;
-      using ComponentT::tag_;
-      using ComponentT::time_;
-      using ComponentT::y_;
-      using ComponentT::yB_;
-      using ComponentT::yp_;
-      using ComponentT::ypB_;
-
-    public:
-      ClassicalGen(BaseBusT* bus, int unit_id, double pmech, double ep)
-        : bus_(bus),
-          unit_id_(unit_id),
-          busID_(0),
-          H_(3),
-          D_(0),
-          Ra_(0),
-          Xdp_(0.2),
-          pmech_(pmech),
-          ep_(ep)
-      {
-        size_ = 5;
-        set_derived_params();
-      }
-
-      ClassicalGen(BaseBusT* bus,
-             int       unit_id,
-             double    H,
-             double    D,
-             double    Ra,
-             double    Xdp,
-             double    pmech,
-             double    ep)
-        : bus_(bus),
-          unit_id_(unit_id),
-          busID_(0),
-          H_(H),
-          D_(D),
-          Ra_(Ra),
-          Xdp_(Xdp),
-          pmech_(pmech),
-          ep_(ep)
-      {
-        size_ = 5;
-        set_derived_params();
-      }
-
-      void set_derived_params()
-      {
-        g  = Ra_ / (Ra_ * Ra_ + Xdp_ * Xdp_);
-        b   = Xdp_ / (Ra_ * Ra_ + Xdp_ * Xdp_);
-      }
-
-      ~ClassicalGen()
-      {
-      }
-
-      int allocate() override
-      {
-        f_.resize(size_);
-        y_.resize(size_);
-        yp_.resize(size_);
-        tag_.resize(size_);
-        fB_.resize(size_);
-        yB_.resize(size_);
-        ypB_.resize(size_);
-        return 0;
-      }
-
-      int initialize() override
-      {
-        return  0;
-      }
-
-      int tagDifferentiable() override
-      {
-        return 0;
-      }
-
-      int evaluateResidual() override
-      {
-        /* Read variables */
-        double delta  = y_[0];
-        double omega  = y_[1];
-        double telec  = y_[2];
-        double ir     = y_[3];
-        double ii     = y_[4];
-
-        /* Read derivatives */
-        double delta_dot = yp_[0];
-        double omega_dot = yp_[1];
-
-        /* 6 GENROU differential equations */
-        f_[0] = delta_dot - omega * (2 * M_PI * 60);
-        f_[1] = omega_dot - (1.0 / (2 * H_)) * ((pmech_ - D_ * omega) / (1 + omega) - telec);
-        
-        /* 11 GENROU algebraic equations */
-        f_[2] = telec - (1.0/(1.0 + omega))*(g*ep_*ep_ - ep_*(cos(delta)*(g*Vr() - b*Vi()) + sin(delta)*(b*Vr() + g*Vi())));
-
-        f_[3] = ir + g*Vr() - b * Vi()  - ep_*(g*cos(delta) -b*sin(delta));
-        f_[4] = ii + b*Vr() +  g * Vi() - ep_*(b*cos(delta) + g*sin(delta));
-
-    
-        /* Current balance */
-        Ir() += - (g*Vr() - b * Vi()  - ep_*(g*cos(delta) -b*sin(delta)));
-        Ii() += - (b*Vr() +  g * Vi() - ep_*(b*cos(delta) + g*sin(delta)));
-
-        // printf("GENROU residual\n");
-        // for (int i = 0 ; i < 21; ++i) printf("%d: %g\n", i, f_[i]);
-
-        // printf("GENROU inr %g Vr %g B %g Vi %g G %g\n", inr, Vr(), B_, Vi(), G_);
-        // printf("GENROU Ii = %g\n", inr - Vr()*B_ - Vi()*G_);
-
-        return 0;
-      }
-
-      int evaluateJacobian() override
-      {
-        /* TODO */
-        return 0;
-      }
-
-      /* Don't know what to do with any of these */
-      int evaluateIntegrand() override
-      {
-        return 0;
-      }
-
-      int initializeAdjoint() override
-      {
-        return 0;
-      }
-
-      int evaluateAdjointResidual() override
-      {
-        return 0;
-      }
-
-      int evaluateAdjointIntegrand() override
-      {
-        return 0;
-      }
-
-      void updateTime(double t, double a) override
-      {
-      }
-
-    private:
-      double& Vr()
-      {
-        return bus_->Vr();
-      }
-
-      double& Vi()
-      {
-        return bus_->Vi();
-      }
-
-      double& Ir()
-      {
-        return bus_->Ir();
-      }
-
-      double& Ii()
-      {
-        return bus_->Ii();
-      }
-
-    private:
-      /* Identification */
-      BaseBusT* bus_;
-      const int busID_;
-      int       unit_id_;
-
-      /* Input parameters */ 
-      double H_;
-      double D_;
-      double Ra_;
-      double Xdp_;
-
-      /* Derivied parameters */
-      double g;
-      double b;
-
-      /* Setpoints for control variables (determined at initialization) */
-      double pmech_;
-      double ep_;
-    };
-
-  } // namespace PhasorDynamics
-} // namespace GridKit
+ #include <Model/PhasorDynamics/Component.hpp>
+ 
+ // Forward declarations.
+ namespace GridKit
+ {
+   namespace PhasorDynamics
+   {
+     template <class ScalarT, typename IdxT>
+     class BusBase;
+   }
+ } // namespace GridKit
+ 
+ namespace GridKit
+ {
+   namespace PhasorDynamics
+   {
+ 
+     template <class ScalarT, typename IdxT>
+     class ClassicalGen : public Component<ScalarT, IdxT>
+     {
+       using Component<ScalarT, IdxT>::alpha_;
+       using Component<ScalarT, IdxT>::f_;
+       using Component<ScalarT, IdxT>::fB_;
+       using Component<ScalarT, IdxT>::g_;
+       using Component<ScalarT, IdxT>::gB_;
+       using Component<ScalarT, IdxT>::nnz_;
+       using Component<ScalarT, IdxT>::param_;
+       using Component<ScalarT, IdxT>::size_;
+       using Component<ScalarT, IdxT>::tag_;
+       using Component<ScalarT, IdxT>::time_;
+       using Component<ScalarT, IdxT>::y_;
+       using Component<ScalarT, IdxT>::yB_;
+       using Component<ScalarT, IdxT>::yp_;
+       using Component<ScalarT, IdxT>::ypB_;
+ 
+       using bus_type  = BusBase<ScalarT, IdxT>;
+       using real_type = typename Component<ScalarT, IdxT>::real_type;
+ 
+     public:
+     ClassicalGen(bus_type* bus, int unit_id);
+     ClassicalGen(bus_type* bus,
+              int       unit_id,
+              real_type H,
+              real_type D,
+              real_type Ra,
+              real_type Xdp,
+              real_type pmech,
+              real_type ep);
+       ~ClassicalGen() = default;
+ 
+       int allocate() override;
+       int initialize() override;
+       int tagDifferentiable() override;
+       int evaluateResidual() override;
+ 
+       // Still to be implemented
+       int evaluateJacobian() override;
+       int evaluateIntegrand() override;
+       int initializeAdjoint() override;
+       int evaluateAdjointResidual() override;
+       int evaluateAdjointIntegrand() override;
+ 
+       void updateTime(real_type /* t */, real_type /* a */) override
+       {
+       }
+ 
+     private:
+       void setDerivedParams();
+ 
+       ScalarT& Vr()
+       {
+         return bus_->Vr();
+       }
+ 
+       ScalarT& Vi()
+       {
+         return bus_->Vi();
+       }
+ 
+       ScalarT& Ir()
+       {
+         return bus_->Ir();
+       }
+ 
+       ScalarT& Ii()
+       {
+         return bus_->Ii();
+       }
+ 
+     private:
+       /* Identification */
+       bus_type* bus_;
+       const int busID_;
+       int       unit_id_;
+ 
+       /* Input parameters */
+       real_type H_;
+       real_type D_;
+       real_type Ra_;
+       real_type Xdp_;
+       
+       /* Derivied parameters */
+       real_type g;
+       real_type b;
+ 
+       /* Setpoints for control variables (determined at initialization) */
+       real_type pmech_;
+       real_type ep_;
+     };
+ 
+   } // namespace PhasorDynamics
+ } // namespace GridKit
+ 
