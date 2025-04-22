b53307b52a7827116590c4ac01212aebd7e9ec6d
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-04-22 12:58:29 -0400

    Compute the analytical Jacobian for BranchTests and verify the answers.

--- tests/UnitTests/PhasorDynamics/BranchTests.hpp
+++ tests/UnitTests/PhasorDynamics/BranchTests.hpp
@@ -92,68 +92,26 @@ namespace GridKit
         Sparse::Variable Vr2{30.0}; ///< Bus-2 real voltage
         Sparse::Variable Vi2{40.0}; ///< Bus-2 imaginary voltage
           
-        Vr1.setVariableNumber(0);
-        Vi1.setVariableNumber(1);
-        Vr2.setVariableNumber(2);
-        Vi2.setVariableNumber(3);
+        Vr1.setVariableNumber(0); ///< Independent variables: first
+        Vi1.setVariableNumber(1); ///< Independent variables: second
+        Vr2.setVariableNumber(2); ///< Independent variables: third
+        Vi2.setVariableNumber(3); ///< Independent variables: fourth
 
         PhasorDynamics::BusInfinite<Sparse::Variable, IdxT> bus1(Vr1, Vi1);
         PhasorDynamics::BusInfinite<Sparse::Variable, IdxT> bus2(Vr2, Vi2);
 
         PhasorDynamics::Branch<Sparse::Variable, IdxT> branch(&bus1, &bus2, R, X, G, B);
-        branch.evaluateResidual();
+        branch.evaluateResidual(); ///< Computes the residual and the Jacobian values by tracking 
+                                   ///< the dependencies
 
+        std::vector<Sparse::Variable> residuals{bus1.Ir(), bus1.Ii(), bus2.Ir(), bus2.Ii()};
+        std::vector<Sparse::Variable::DependencyMap> ref = analyticalJacobian(R, X, G, B);
+        for (size_t i = 0; i < residuals.size(); ++i)
         {
-          Sparse::Variable res = bus1.Ir();
-          const Sparse::Variable::DependencyMap& dependencies =
-              res.getDependencies();
-
-          std::cout << "Dependency size: " << dependencies.size() << "\n";
-          success *= (dependencies.size() == 4);
-          for (const auto& [key, value] : dependencies)
-          {
-            std::cout << '[' << key << "] = " << value << "; ";
-          }
-          std::cout << std::endl;
-        }
-        {
-          Sparse::Variable res = bus1.Ii();
-          const Sparse::Variable::DependencyMap& dependencies =
-              res.getDependencies();
-
-          std::cout << "Dependency size: " << dependencies.size() << "\n";
-          success *= (dependencies.size() == 4);
-          for (const auto& [key, value] : dependencies)
-          {
-            std::cout << '[' << key << "] = " << value << "; ";
-          }
-          std::cout << std::endl;
-        }
-        {
-          Sparse::Variable res = bus2.Ir();
-          const Sparse::Variable::DependencyMap& dependencies =
-              res.getDependencies();
-
-          std::cout << "Dependency size: " << dependencies.size() << "\n";
-          success *= (dependencies.size() == 4);
-          for (const auto& [key, value] : dependencies)
-          {
-            std::cout << '[' << key << "] = " << value << "; ";
-          }
-          std::cout << std::endl;
-        }
-        {
-          Sparse::Variable res = bus2.Ii();
-          const Sparse::Variable::DependencyMap& dependencies =
-              res.getDependencies();
-
-          std::cout << "Dependency size: " << dependencies.size() << "\n";
-          success *= (dependencies.size() == 4);
-          for (const auto& [key, value] : dependencies)
-          {
-            std::cout << '[' << key << "] = " << value << "; ";
-          }
-          std::cout << std::endl;
+          Sparse::Variable res = residuals[i];
+          const Sparse::Variable::DependencyMap& dependencies = res.getDependencies();
+          success *= (dependencies == ref[i]); ///< Compare dependencies computed automatically
+                                               ///< to the ones computed analytically
         }
 
         return success.report(__func__);
@@ -220,6 +178,43 @@ namespace GridKit
         branch.setB(zero);
 
         return success.report(__func__);
+      }
+    private:
+      std::vector<Sparse::Variable::DependencyMap> analyticalJacobian(const real_type R,
+                                                                      const real_type X,
+                                                                      const real_type G,
+                                                                      const real_type B)
+      {
+        const real_type b = -X / (R * R + X * X);
+        const real_type g = R / (R * R + X * X);
+        
+        real_type dIr1_dVr1 = -(g + 0.5 * G);
+        real_type dIr1_dVi1 = (b + 0.5 * B);
+        real_type dIr1_dVr2 = g;
+        real_type dIr1_dVi2 = -b;
+
+        real_type dIi1_dVr1 = -(b + 0.5 * B); 
+        real_type dIi1_dVi1 = -(g + 0.5 * G);
+        real_type dIi1_dVr2 = b;
+        real_type dIi1_dVi2 = g;
+                              
+        real_type dIr2_dVr1 = g; 
+        real_type dIr2_dVi1 = -b;
+        real_type dIr2_dVr2 = -(g + 0.5 * G);
+        real_type dIr2_dVi2 = (b + 0.5 * B);
+                              
+        real_type dIi2_dVr1 = b; 
+        real_type dIi2_dVi1 = g;
+        real_type dIi2_dVr2 = -(b + 0.5 * B);
+        real_type dIi2_dVi2 = -(g + 0.5 * G);
+
+        std::vector<Sparse::Variable::DependencyMap> dependencies(4);
+        dependencies[0] = {{0, dIr1_dVr1}, {1, dIr1_dVi1}, {2, dIr1_dVr2}, {3, dIr1_dVi2}};
+        dependencies[1] = {{0, dIi1_dVr1}, {1, dIi1_dVi1}, {2, dIi1_dVr2}, {3, dIi1_dVi2}};
+        dependencies[2] = {{0, dIr2_dVr1}, {1, dIr2_dVi1}, {2, dIr2_dVr2}, {3, dIr2_dVi2}};
+        dependencies[3] = {{0, dIi2_dVr1}, {1, dIi2_dVi1}, {2, dIi2_dVr2}, {3, dIi2_dVi2}};
+
+        return dependencies;
       }
     }; // class BranchTest
 
