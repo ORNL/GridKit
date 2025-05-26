884c8e382af406ec627f6601244b2a39f2476661
Slaven Peles <peless@ornl.gov>
2025-05-26 18:13:53 -0400

    Move CircuitGraph.hpp to Model/PowerElectronics

--- src/Model/PowerElectronics/SystemModelPowerElectronics.hpp
+++ src/Model/PowerElectronics/SystemModelPowerElectronics.hpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <vector>
 
-#include <CircuitGraph.hpp>
+#include <Model/PowerElectronics/CircuitGraph.hpp>
 #include <Model/PowerElectronics/CircuitComponent.hpp>
 #include <ScalarTraits.hpp>
 
