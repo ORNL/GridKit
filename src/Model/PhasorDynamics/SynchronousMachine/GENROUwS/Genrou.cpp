6e69376db5f9e28bcec3869d483cb814b9649bdf
Luke Lowery <wyattluke.lowery@gmail.com>
2025-06-26 17:49:27 -0500

    Example for Tgov1 (#156)
    ---------
    
    Co-authored-by: lukelowry <lukelowry@users.noreply.github.com>
    Co-authored-by: Adam Birchfield <adam.b.birchfield@gmail.com>
    Co-authored-by: pelesh <peless@ornl.gov>

--- src/Model/PhasorDynamics/SynchronousMachine/GENROUwS/Genrou.cpp
+++ src/Model/PhasorDynamics/SynchronousMachine/GENROUwS/Genrou.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 
 #include <Model/PhasorDynamics/Bus/Bus.hpp>
+#include <Model/PhasorDynamics/Governor/Tgov1/Tgov1.hpp> // <- TODO: Temporary, to be removed.
 #include <Model/PhasorDynamics/SynchronousMachine/GENROUwS/GenrouData.hpp>
 
 #define _USE_MATH_DEFINES
@@ -36,6 +37,7 @@ namespace GridKit
       : bus_(bus),
         busID_(0),
         unit_id_(unit_id),
+        gov_(nullptr), // <- TODO: Temporary, to be removed.
         p0_(0.),
         q0_(0.),
         H_(3.),
@@ -55,7 +57,7 @@ namespace GridKit
         S10_(0.),
         S12_(0.)
     {
-      size_ = 21;
+      size_ = 20;
       setDerivedParams();
 
       // Temporary, to eliminate compiler warnings
@@ -91,6 +93,7 @@ namespace GridKit
       : bus_(bus),
         busID_(0),
         unit_id_(unit_id),
+        gov_(nullptr), // <- TODO: Temporary, to be removed.
         p0_(p0),
         q0_(q0),
         H_(H),
@@ -110,7 +113,7 @@ namespace GridKit
         S10_(S10),
         S12_(S12)
     {
-      size_ = 21;
+      size_ = 20;
       setDerivedParams();
     }
 
@@ -123,6 +126,7 @@ namespace GridKit
       : bus_(bus),
         busID_(0),
         unit_id_(data.unit_id),
+        gov_(nullptr), // <- TODO: Temporary, to be removed.
         p0_(data.p0),
         q0_(data.q0),
         H_(data.H),
@@ -142,7 +146,7 @@ namespace GridKit
         S10_(data.S10),
         S12_(data.S12)
     {
-      size_ = 21;
+      size_ = 20;
       setDerivedParams();
     }
 
@@ -229,12 +233,14 @@ namespace GridKit
       y_[14]      = iq;
       y_[15]      = ir;
       y_[16]      = ii;
-      y_[17] = pmech_set_ = Te;
-      y_[18] = efd_set_ = Eqp + Xd1_ * (id + Xd3_ * (Eqp - psidp - Xd2_ * id)) + psidpp * ksat;
-      y_[19]            = G_ * (vd * sin(delta) + vq * cos(delta))
+      y_[17] = efd_set_ = Eqp + Xd1_ * (id + Xd3_ * (Eqp - psidp - Xd2_ * id)) + psidpp * ksat;
+      y_[18]            = G_ * (vd * sin(delta) + vq * cos(delta))
                - B_ * (vd * -cos(delta) + vq * sin(delta)); /* inort, real */
-      y_[20] = B_ * (vd * sin(delta) + vq * cos(delta))
+      y_[19] = B_ * (vd * sin(delta) + vq * cos(delta))
                + G_ * (vd * -cos(delta) + vq * sin(delta)); /* inort, imag */
+
+      // Set Setpoint mechanical power, which may or may not be used
+      pmech_set_ = Te;
 
       for (IdxT i = 0; i < size_; ++i)
         yp_[static_cast<size_t>(i)] = 0.0;
@@ -281,12 +287,20 @@ namespace GridKit
       ScalarT iq     = y_[14];
       ScalarT ir     = y_[15];
       ScalarT ii     = y_[16];
-      ScalarT pmech  = y_[17];
-      ScalarT efd    = y_[18];
-      ScalarT inr    = y_[19];
-      ScalarT ini    = y_[20];
+      ScalarT efd    = y_[17];
+      ScalarT inr    = y_[18];
+      ScalarT ini    = y_[19];
       ScalarT vr     = Vr();
       ScalarT vi     = Vi();
+      ScalarT pmech;
+      if (gov_)
+      {
+        pmech = gov_->Pmech();
+      }
+      else
+      {
+        pmech = pmech_set_;
+      }
 
       /* Read derivatives */
       ScalarT delta_dot = yp_[0];
@@ -318,12 +332,11 @@ namespace GridKit
       f_[16] = ii + B_ * vr + G_ * vi - ini;
 
       /* 2 Genrou control inputs are set to constant for this example */
-      f_[17] = pmech - pmech_set_;
-      f_[18] = efd - efd_set_;
+      f_[17] = efd - efd_set_;
 
       /* 2 Genrou current source definitions */
-      f_[19] = inr - (G_ * (sin(delta) * vd + cos(delta) * vq) - B_ * (-cos(delta) * vd + sin(delta) * vq));
-      f_[20] = ini - (B_ * (sin(delta) * vd + cos(delta) * vq) + G_ * (-cos(delta) * vd + sin(delta) * vq));
+      f_[18] = inr - (G_ * (sin(delta) * vd + cos(delta) * vq) - B_ * (-cos(delta) * vd + sin(delta) * vq));
+      f_[19] = ini - (B_ * (sin(delta) * vd + cos(delta) * vq) + G_ * (-cos(delta) * vd + sin(delta) * vq));
 
       /* Current balance */
       Ir() += inr - Vr() * G_ + Vi() * B_;
@@ -420,6 +433,12 @@ namespace GridKit
     ScalarT Genrou<ScalarT, IdxT>::getTorque()
     {
       return y_[12];
+    }
+
+    template <class ScalarT, typename IdxT>
+    void Genrou<ScalarT, IdxT>::setgovenor(gov_type* gov)
+    {
+      gov_ = gov;
     }
 
     template <class ScalarT, typename IdxT>
