8711844cc8162ba187e9b240295642188b2a3889
superwhiskers <superwhiskers@users.noreply.github.com>
2025-06-27 14:49:44 +0000

    Apply pre-commmit fixes

--- src/Model/PowerElectronics/SystemModelPowerElectronics.hpp
+++ src/Model/PowerElectronics/SystemModelPowerElectronics.hpp
@@ -3,9 +3,9 @@
 #pragma once
 
 #include <cassert>
+#include <iomanip>
 #include <iostream>
 #include <vector>
-#include <iomanip>
 
 #include <Model/PowerElectronics/CircuitComponent.hpp>
 #include <Model/PowerElectronics/CircuitGraph.hpp>
