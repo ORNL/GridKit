4a03feeb45370c7f79603249ea1fc9710cbc5b6d
Slaven Peles <peless@ornl.gov>
2025-07-01 18:43:51 -0400

    Sparse Jacobian allocates nnz instead of n^2 elements.

--- src/Solver/Dynamic/Ida.cpp
+++ src/Solver/Dynamic/Ida.cpp
@@ -116,9 +116,10 @@ namespace AnalysisManager
       int retval = 0;
       if (model_->hasJacobian())
       {
+        sunindextype nnz = static_cast<sunindextype>((model_->getJacobian()).nnz());
         JacobianMat_ = SUNSparseMatrix(static_cast<sunindextype>(model_->size()),
                                        static_cast<sunindextype>(model_->size()),
-                                       static_cast<sunindextype>(model_->size() * model_->size()),
+                                       nnz,
                                        CSR_MAT,
                                        context_);
         checkAllocation((void*) JacobianMat_, "SUNSparseMatrix");
@@ -589,7 +590,7 @@ namespace AnalysisManager
       copyVec(yp, model->yp());
 
       model->evaluateJacobian();
-      GridKit::LinearAlgebra::COO_Matrix<ScalarT, IdxT> Jac = model->getJacobian();
+      GridKit::LinearAlgebra::COO_Matrix<ScalarT, IdxT>& Jac = model->getJacobian();
 
       // Get reference to the jacobian entries
       std::tuple<std::vector<IdxT>&, std::vector<IdxT>&, std::vector<ScalarT>&> tpm = Jac.getEntries();
