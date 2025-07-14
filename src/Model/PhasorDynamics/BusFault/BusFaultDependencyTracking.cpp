ef7f61663db3006ca6c80b634d91b6a49ec779a2
superwhiskers <superwhiskers@users.noreply.github.com>
2025-07-14 18:49:41 +0000

    Apply pre-commmit fixes

--- src/Model/PhasorDynamics/BusFault/BusFaultDependencyTracking.cpp
+++ src/Model/PhasorDynamics/BusFault/BusFaultDependencyTracking.cpp
@@ -7,5 +7,5 @@ namespace GridKit
     // Additional template instantiations
     template class BusFault<DependencyTracking::Variable, long int>;
     template class BusFault<DependencyTracking::Variable, size_t>;
-  }
-}
+  } // namespace PhasorDynamics
+} // namespace GridKit
