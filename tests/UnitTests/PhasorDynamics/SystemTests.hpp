1f36f64d9475b86fb3907eccf4be0b5af6aaedf9
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2025-07-15 08:37:54 -0400

    Unit test update for system Jacobian testing (#183)
    * Verify dependencies obtained in the unit test through the system model.
    
    * Remove print statement from SystemTests::dependencyTracking.
    
    * Apply pre-commmit fixes.

--- tests/UnitTests/PhasorDynamics/SystemTests.hpp
+++ tests/UnitTests/PhasorDynamics/SystemTests.hpp
@@ -150,6 +150,21 @@ namespace GridKit
         system.allocate();
         system.initialize();
         system.evaluateResidual();
+        std::vector<DependencyTracking::Variable> residuals{bus0->Ir(), bus0->Ii(), bus1->Ir(), bus1->Ii()};
+
+        /// Verify the size and structure of the residual dependencies.
+        /// We expect the Jacobian of the branch (dense 4x4).
+        /// We are less concerned with the values here, as the goal is to get the sparsity pattern.
+        for (size_t i = 0; i < residuals.size(); ++i)
+        {
+          DependencyTracking::Variable                       res           = residuals[i];
+          const DependencyTracking::Variable::DependencyMap& dependencies  = res.getDependencies();
+          success                                                         *= (dependencies.size() == 4);
+          success                                                         *= (dependencies.find(0) != dependencies.end());
+          success                                                         *= (dependencies.find(1) != dependencies.end());
+          success                                                         *= (dependencies.find(2) != dependencies.end());
+          success                                                         *= (dependencies.find(3) != dependencies.end());
+        }
 
         return success.report(__func__);
       }
