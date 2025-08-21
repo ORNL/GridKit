d9ca8467d23a84d9c9885f9d2487360811531926
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-08-21 16:56:28 -0400

    Revert attempt to fix the dynamic solver build when ~klu.

--- src/Solver/Dynamic/Ida.hpp
+++ src/Solver/Dynamic/Ida.hpp
@@ -11,14 +11,9 @@
 #include <sunlinsol/sunlinsol_dense.h>  /* access to dense linear solver        */
 #include <sunmatrix/sunmatrix_sparse.h> /* access to sparse SUNMatrix           */
 
-#include <Definitions.hpp>
-
-#ifdef GRIDKIT_ENABLE_SUNDIALS_SPARSE
-#include <sunlinsol/sunlinsol_klu.h> /* access to KLU linear solver          */
-#endif
-
 #include "DynamicSolver.hpp"
 #include "Model/Evaluator.hpp"
+#include <Definitions.hpp>
 
 namespace AnalysisManager
 {
