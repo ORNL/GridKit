77b444ef6d7416be157e17ad23a0e202c70080cd
pelesh <peless@ornl.gov>
2025-09-10 17:56:51 -0400

    Added governor to the parser test.

--- tests/UnitTests/Utilities/CaseFormatTests.hpp
+++ tests/UnitTests/Utilities/CaseFormatTests.hpp
@@ -3,6 +3,7 @@
 #include <Model/PhasorDynamics/Branch/BranchData.hpp>
 #include <Model/PhasorDynamics/Bus/BusData.hpp>
 #include <Model/PhasorDynamics/BusFault/BusFaultData.hpp>
+#include <Model/PhasorDynamics/Governor/Tgov1/Tgov1Data.hpp>
 #include <Model/PhasorDynamics/SynchronousMachine/GENROUwS/GenrouData.hpp>
 #include <Model/PhasorDynamics/SystemModelData.hpp>
 #include <Model/PhasorDynamics/SystemModelDataJSONParser.hpp>
@@ -157,7 +158,7 @@ namespace GridKit
                "devices": [
                    { "class": "Branch", "ports": {"bus1":1, "bus2":2}, "id": "BR1", "params": {"R":0.0, "X":0.1, "G":0.0, "B":0.0} },
                    { "class": "Genrou", "ports": {"bus":1, "speed": 1, "pmech":2, "efd":3}, "id": "DV1", "params": {"p0":1.0, "q0":0.05013, "H":3.0, "D":0.0, "Ra":0.0, "Tdop":7.0, "Tdopp":0.04, "Tqopp":0.05, "Tqop":0.75, "Xd":2.1, "Xdp":0.2, "Xdpp":0.18, "Xq":0.5, "Xqp": 0.0, "Xqpp":0.18, "Xl":0.15, "S10":0.0, "S12":0.0}, "mon": ["delta", "omega"] },
-                   { "class": "Tgov1", "ports": {"bus":1, "speed": 1, "pmech":2}, "id": "DV2", "params": {"R":0.05, "T1":0.5,"T2":2.5, "T3":7.5, "Pvmax":0, "Pvmin":1, "Dt":0}},
+                   { "class": "Tgov1", "ports": {"bus":1, "speed": 1, "pmech":2}, "id": "DV2", "params": {"R":0.05, "T1":0.5,"T2":2.5, "T3":7.5, "Pvmax":0.0, "Pvmin":1.0, "Dt":0.0}},
                    { "class": "bus_fault", "ports": {"bus":1}, "id": "1", "params": {"state0": false, "R":0.0, "X":1e-3} }
                ]
             })";
@@ -178,6 +179,7 @@ namespace GridKit
         success *= result.branch.size() == 1;
         success *= result.bus_fault.size() == 1;
         success *= result.genrou.size() == 1;
+        success *= result.gov.size() == 1;
         success *= result.load.size() == 0;
 
         success *= result.bus[0].bus_id == 1;
@@ -231,9 +233,24 @@ namespace GridKit
         success *= std::get<RealT>(result.genrou[0].parameters[GenrouParameters::S10]) == 0.0;
         success *= std::get<RealT>(result.genrou[0].parameters[GenrouParameters::S12]) == 0.0;
         success *= result.genrou[0].ports[GenrouPorts::bus] == 1;
+        success *= result.genrou[0].ports[GenrouPorts::speed] == 1;
+        success *= result.genrou[0].ports[GenrouPorts::pmech] == 2;
+        success *= result.genrou[0].ports[GenrouPorts::efd] == 3;
         success *= result.genrou[0].disambiguation_string == "DV1";
         success *= result.genrou[0].monitored_variables.contains(GenrouMonitorableVariables::delta);
         success *= result.genrou[0].monitored_variables.contains(GenrouMonitorableVariables::omega);
+
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::R]) == 0.05;
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::T1]) == 0.5;
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::T2]) == 2.5;
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::T3]) == 7.5;
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::Pvmax]) == 0;
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::Pvmin]) == 1;
+        success *= std::get<RealT>(result.gov[0].parameters[Governor::Tgov1Parameters::Dt]) == 0;
+        success *= result.gov[0].ports[Governor::Tgov1Ports::bus] == 1;
+        success *= result.gov[0].ports[Governor::Tgov1Ports::speed] == 1;
+        success *= result.gov[0].ports[Governor::Tgov1Ports::pmech] == 2;
+        success *= result.gov[0].disambiguation_string == "DV2";
 
         success *= std::get<RealT>(result.bus_fault[0].parameters[BusFaultParameters::R]) == 0.0;
         success *= std::get<RealT>(result.bus_fault[0].parameters[BusFaultParameters::X]) == 1e-3;
