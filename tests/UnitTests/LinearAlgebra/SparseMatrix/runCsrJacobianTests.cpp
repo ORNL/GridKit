07fd122b294822b2be842578cb97889ecfb23de6
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2025-09-12 11:57:45 -0400

    [WIP]

--- tests/UnitTests/LinearAlgebra/SparseMatrix/runCsrJacobianTests.cpp
+++ tests/UnitTests/LinearAlgebra/SparseMatrix/runCsrJacobianTests.cpp
@@ -10,13 +10,10 @@ int main()
   using namespace GridKit::LinearAlgebra;
 
   TestingResults                   result;
-  CsrJacobianTests<double, size_t> test;
+  CSRJacobianTests<double, size_t> test;
 
   result += test.testCooVsCsrJacobian<Resistor>("Resistor", 1, 1.0);
   result += test.testCooVsCsrJacobian<Resistor>("Resistor", 1, 2.0);
-
-  auto rl_circuit_system  = rlCircuitSystem(1.0e-8, 1.0e-8, true, 1.0, 1.0, 1.0);
-  result                 += test.testSystemCooVsCsrJacobian("RLCircuit", *rl_circuit_system);
 
   return result.summary();
 }
