4abdf7ed223c91a591fd8c7732798c5b0682feaa
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-09-26 17:00:57 -0400

    Fix inline warnings.

--- src/Model/PhasorDynamics/Load/Load.hpp
+++ src/Model/PhasorDynamics/Load/Load.hpp
@@ -95,7 +95,7 @@ namespace GridKit
       }
 
     public:
-      __attribute__((always_inline)) int evaluateResidualLocally(ScalarT*, ScalarT*, ScalarT*);
+      __attribute__((always_inline)) inline int evaluateResidualLocally(ScalarT*, ScalarT*, ScalarT*);
 
     private:
       bus_type* bus_{nullptr};
