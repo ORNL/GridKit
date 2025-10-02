50edb3832d37a8054be38854f0a6b501943a8e79
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2025-10-02 18:03:48 -0400

    Specify inline in function definition as well
    Co-authored-by: Steven Roberts <sroberts994@gmail.com>

--- src/Model/PhasorDynamics/Branch/BranchImpl.hpp
+++ src/Model/PhasorDynamics/Branch/BranchImpl.hpp
@@ -191,7 +191,7 @@ namespace GridKit
      *
      */
     template <class ScalarT, typename IdxT>
-    __attribute__((always_inline)) int Branch<ScalarT, IdxT>::evaluateBusResidual12(
+    __attribute__((always_inline)) inline int Branch<ScalarT, IdxT>::evaluateBusResidual12(
         [[maybe_unused]] ScalarT* y,
         [[maybe_unused]] ScalarT* yp,
         ScalarT*                  w,
