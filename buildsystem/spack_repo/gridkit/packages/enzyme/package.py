f129453c8845cb1cf3219b6b06e040dcb603880b
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2025-10-07 15:53:23 -0400

    Add enzyme@0.0.203 to our repo.

--- buildsystem/spack_repo/gridkit/packages/enzyme/package.py
+++ buildsystem/spack_repo/gridkit/packages/enzyme/package.py
@@ -3,5 +3,6 @@ from spack_repo.builtin.packages.enzyme.package import Enzyme as BuiltinEnzyme
 from spack.package import *
 
 class Enzyme(BuiltinEnzyme):
+    version("0.0.203", sha256="2135f8836fccf697112554abb67acc7860b7a21db2c4af50d5be6528755beecf")
     version("0.0.196", sha256="2b9cfcb7c34e56fc8191423042df06241cf32928eefbb113ac3c5199e3361cb2")
     version("0.0.186", sha256="125e612df0b6b82b07e1e13218c515bc54e04aa1407e57f4f31d3abe995f4714")
