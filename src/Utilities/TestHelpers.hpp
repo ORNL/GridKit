4104e7e8fa3aa5d705f4496e1df21f5f258df0a3
Philip Fackler <facklerpw@ornl.gov>
2025-10-16 11:48:22 -0500

    Introduce Logger class with tests

--- src/Utilities/TestHelpers.hpp
+++ src/Utilities/TestHelpers.hpp
@@ -13,21 +13,10 @@
 #include <iostream>
 #include <limits>
 
+#include <Utilities/Colors.hpp>
+
 namespace GridKit
 {
-
-  namespace Colors
-  {
-    // must be const pointer and const dest for
-    // const string declarations to pass -Wwrite-strings
-    static const char* const RED    = "\033[1;31m";
-    static const char* const GREEN  = "\033[1;32m";
-    static const char* const YELLOW = "\033[33;1m";
-    static const char* const BLUE   = "\033[34;1m";
-    static const char* const ORANGE = "\u001b[38;5;208m";
-    static const char* const CLEAR  = "\033[0m";
-  } // namespace Colors
-
   namespace Testing
   {
     enum TestOutcome
@@ -95,7 +84,7 @@ namespace GridKit
 
       TestOutcome report(const char* funcname)
       {
-        using namespace Colors;
+        using namespace Utilities::Colors;
 
         if (expectFailure_)
         {
