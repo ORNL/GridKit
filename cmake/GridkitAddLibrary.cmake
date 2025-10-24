648699727855c80118f4dc0e395b7a8b09a5ef7f
Philip Fackler <facklerpw@ornl.gov>
2025-10-24 15:37:29 -0500

    Install public headers

--- cmake/GridkitAddLibrary.cmake
+++ cmake/GridkitAddLibrary.cmake
@@ -51,7 +51,7 @@ macro(gridkit_add_library target)
   if(gridkit_add_library_INCLUDE_DIRECTORIES)
     target_include_directories(${target} PUBLIC
       $<BUILD_INTERFACE:${gridkit_add_library_INCLUDE_DIRECTORIES}>
-      $<INSTALL_INTERFACE:include>)
+      $<INSTALL_INTERFACE:include/GridKit>)
   endif()
 
   # add compile options
@@ -82,7 +82,7 @@ macro(gridkit_add_library target)
       BASE_DIRECTORY ${CMAKE_SOURCE_DIR}/src
       OUTPUT_VARIABLE _rel_path)
     install(FILES ${gridkit_add_library_HEADERS}
-      DESTINATION include/${_rel_path})
+      DESTINATION include/GridKit/${_rel_path})
   endif()
 
 endmacro()
