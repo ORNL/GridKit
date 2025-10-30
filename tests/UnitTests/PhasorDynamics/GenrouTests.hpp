491ea31333c658e9648b5430abbf7e6ee93815e7
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2025-10-30 16:21:24 -0400

    Re-activate GenrouEnzyme (#293)
    * Enable GenrouEnzyme.
    
    * Used enzyme@0.0.206.

--- tests/UnitTests/PhasorDynamics/GenrouTests.hpp
+++ tests/UnitTests/PhasorDynamics/GenrouTests.hpp
@@ -220,7 +220,6 @@ namespace GridKit
         return success.report(__func__);
       }
 
-#if 0 // Disabled GenrouEnzyme because of an intermittent build issue
 #ifdef GRIDKIT_ENABLE_ENZYME
       /**
        * @brief Checks Jacobian evaluation.
@@ -342,7 +341,6 @@ namespace GridKit
 
         return GridKit::Testing::MapFromCOO(model_jacobian);
       }
-#endif
 #endif
     }; // class GenrouTest
 
