5329702c48b097e23065ac4226fe3e5865663135
lukelowry <lukelowry@users.noreply.github.com>
2025-11-24 22:34:53 +0000

    Apply pre-commmit fixes

--- GridKit/Model/PhasorDynamics/Exciter/IEEET1/Ieeet1Impl.hpp
+++ GridKit/Model/PhasorDynamics/Exciter/IEEET1/Ieeet1Impl.hpp
@@ -340,8 +340,8 @@ namespace GridKit
         f_[6] = -ve + ksat * efdp;
         f_[7] = -efd + efdp + omega * efdp * Ispdlim_;
 
-        ScalarT efd_sat  = (efdp - SA_) * ( this->sigmoid(efdp-SA_) );
-        f_[8] = -ksat + SB_ * efd_sat * efd_sat;
+        ScalarT efd_sat = (efdp - SA_) * (this->sigmoid(efdp - SA_));
+        f_[8]           = -ksat + SB_ * efd_sat * efd_sat;
 
         return 0;
       }
