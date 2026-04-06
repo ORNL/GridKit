a3b0901aeaddabdc19cc5f8aab66f07a356f8ae3
abirchfield <adam.b.birchfield@gmail.com>
2026-04-06 14:18:14 -0500

    initial tests

---
+++ tests/UnitTests/PhasorDynamics/runLoadZIPTests.cpp
@@ -0,0 +1,15 @@
+#include "LoadZIPTests.hpp"
+
+int main()
+{
+  using namespace GridKit;
+  using namespace GridKit::Testing;
+
+  GridKit::Testing::TestingResults            result;
+  GridKit::Testing::LoadZIPTests<double, size_t> test;
+
+  result += test.constructor();
+  result += test.residual();
+
+  return result.summary();
+}
