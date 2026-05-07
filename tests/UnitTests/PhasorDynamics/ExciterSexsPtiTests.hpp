1878f52161ecff3e36509d078dbf99488f4698db
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2026-05-07 13:48:07 -0400

    Add Jacobian test for ExciterSexsPti.

--- tests/UnitTests/PhasorDynamics/ExciterSexsPtiTests.hpp
+++ tests/UnitTests/PhasorDynamics/ExciterSexsPtiTests.hpp
@@ -13,6 +13,7 @@
 #include <GridKit/Model/PhasorDynamics/SystemModelData.hpp>
 #include <GridKit/Testing/TestHelpers.hpp>
 #include <GridKit/Testing/Testing.hpp>
+#include <GridKit/Utilities/MapFromCOO.hpp>
 
 namespace GridKit
 {
@@ -288,6 +289,160 @@ namespace GridKit
 
         return success.report(__func__);
       }
+
+#ifdef GRIDKIT_ENABLE_ENZYME
+      /**
+       * @brief Checks Jacobian evaluation.
+       */
+      TestOutcome jacobian()
+      {
+        TestStatus success = true;
+
+        auto tol = 10 * std::numeric_limits<RealT>::epsilon();
+
+        // Jacobian via DependencyTracking
+        std::vector<DependencyTracking::Variable::DependencyMap> dependency_tracking_jacobian = DependencyTrackingJacobian();
+
+        // Jacobian via Enzyme
+        std::vector<DependencyTracking::Variable::DependencyMap> enzyme_jacobian = EnzymeJacobian();
+
+        /// Compare DependencyTracking dependencies to Enzyme's
+        for (size_t i = 0; i < dependency_tracking_jacobian.size(); ++i)
+        {
+          success *= (GridKit::Testing::isEqual(dependency_tracking_jacobian[i], enzyme_jacobian[i], tol));
+        }
+
+        return success.report(__func__);
+      }
+
+    private:
+      std::vector<DependencyTracking::Variable::DependencyMap> DependencyTrackingJacobian()
+      {
+        auto data = makeTestData();
+
+        DependencyTracking::Variable                                         Vr1{3.0};
+        DependencyTracking::Variable                                         Vi1{4.0};
+        PhasorDynamics::Bus<DependencyTracking::Variable, IdxT>              bus(Vr1, Vi1);
+        PhasorDynamics::Exciter::SexsPti<DependencyTracking::Variable, IdxT> exciter(&bus, data);
+
+        bus.allocate();
+        exciter.allocate();
+
+        // Get d/dy
+        bus.initialize();
+        exciter.initialize();
+
+        for (size_t i = 0; i < exciter.size(); ++i)
+        {
+          exciter.y()[i].setVariableNumber(i); ///< Exciter independent variables
+        }
+        for (size_t i = 0; i < bus.size(); ++i)
+        {
+          bus.y()[i].setVariableNumber(i + exciter.size()); // Bus independent variables
+        }
+
+        bus.evaluateResidual();
+        exciter.evaluateResidual(); ///< Computes the residual and the Jacobian values by tracking
+                                    ///< the dependencies
+        std::vector<DependencyTracking::Variable> residual_y = exciter.getResidual();
+
+        // Get d/dy'
+        bus.initialize();
+        exciter.initialize();
+
+        for (size_t i = 0; i < exciter.size(); ++i)
+        {
+          exciter.yp()[i].setVariableNumber(i); ///< Exciter independent variables
+        }
+
+        bus.evaluateResidual();
+        exciter.evaluateResidual(); ///< Computes the residual and the Jacobian values by tracking
+                                    ///< the dependencies
+        std::vector<DependencyTracking::Variable> residual_yp = exciter.getResidual();
+
+        // Print the dependencies
+        for (size_t i = 0; i < residual_y.size(); ++i)
+        {
+          std::cout << i << "th residual, y: ";
+          (residual_y[i]).print(std::cout);
+          std::cout << "\n";
+          std::cout << i << "th residual, yp: ";
+          (residual_yp[i]).print(std::cout);
+          std::cout << "\n";
+        }
+
+        // Extract the dependencies and add d/dy' to d/dy
+        std::vector<DependencyTracking::Variable::DependencyMap> dependencies(residual_y.size());
+        for (IdxT i = 0; i < residual_y.size(); ++i)
+        {
+          DependencyTracking::Variable::DependencyMap dependency_y  = (residual_y[i]).getDependencies();
+          DependencyTracking::Variable::DependencyMap dependency_yp = (residual_yp[i]).getDependencies();
+
+          for (const auto& pair_y : dependency_y)
+          {
+            auto index_y = pair_y.first;
+            auto value_y = pair_y.second;
+            auto it_yp   = dependency_yp.find(index_y);
+            if (it_yp != dependency_yp.end())
+            {
+              auto value_yp = it_yp->second;
+              dependencies[i].insert(std::make_pair(index_y, value_y + value_yp));
+            }
+            else
+            {
+              dependencies[i].insert(std::make_pair(index_y, value_y));
+            }
+          }
+
+          // Insert yp dependencies that did not exist in the y dependencies
+          for (const auto& pair_yp : dependency_yp)
+          {
+            auto index_yp = pair_yp.first;
+            auto value_yp = pair_yp.second;
+            auto it_y     = dependency_y.find(index_yp);
+            if (it_y == dependency_y.end())
+            {
+              dependencies[i].insert(std::make_pair(index_yp, value_yp));
+            }
+          }
+        }
+
+        return dependencies;
+      }
+
+      std::vector<DependencyTracking::Variable::DependencyMap> EnzymeJacobian()
+      {
+        auto data = makeTestData();
+
+        PhasorDynamics::Bus<ScalarT, IdxT>              bus(3.0, 4.0);
+        PhasorDynamics::Exciter::SexsPti<ScalarT, IdxT> exciter(&bus, data);
+
+        bus.allocate();
+        exciter.allocate();
+
+        bus.initialize();
+        exciter.initialize();
+
+        exciter.updateTime(0.0, 1.0); // Set alpha to 1.0 to verify d/dy' term
+
+        for (size_t i = 0; i < bus.size(); ++i)
+        {
+          bus.setVariableIndex(i, i + exciter.size()); // Reset bus variable indices
+          bus.setResidualIndex(i, i + exciter.size()); // Reset bus residual indices
+        }
+
+        bus.evaluateResidual();
+        exciter.evaluateResidual();
+
+        bus.evaluateJacobian();
+        exciter.evaluateJacobian();
+        GridKit::LinearAlgebra::COO_Matrix<ScalarT, IdxT>& model_jacobian = exciter.getJacobian();
+        model_jacobian.deduplicate();
+        model_jacobian.printMatrix("Model Jacobian");
+
+        return GridKit::Testing::MapFromCOO(model_jacobian);
+      }
+#endif
 
     private:
       auto makeTestData() -> PhasorDynamics::Exciter::SexsPtiData<RealT, IdxT>
