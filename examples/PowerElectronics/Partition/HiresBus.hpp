3400ce3b6dac3d51a9d09440d9761f56b30c3463
abdourahmanbarry <abdourahman@vt.edu>
2026-06-01 15:32:09 +0000

    Added example to demonstrate new partitioning scheming for power electronics

---
+++ examples/PowerElectronics/Partition/HiresBus.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <GridKit/Model/PowerElectronics/CircuitComponent.hpp>
+
+namespace GridKit
+{
+  /*!
+   * @brief Declaration of a MicrogridLine class.
+   *
+   */
+  template <class ScalarT, typename IdxT>
+  class HiresBus : public CircuitComponent<ScalarT, IdxT>
+  {
+    using RealT   = typename CircuitComponent<ScalarT, IdxT>::RealT;
+    using MatrixT = typename CircuitComponent<RealT, IdxT>::MatrixT;
+
+    using CircuitComponent<ScalarT, IdxT>::size_;
+    using CircuitComponent<ScalarT, IdxT>::nnz_;
+    using CircuitComponent<ScalarT, IdxT>::time_;
+    using CircuitComponent<ScalarT, IdxT>::alpha_;
+    using CircuitComponent<ScalarT, IdxT>::y_;
+    using CircuitComponent<ScalarT, IdxT>::yp_;
+    using CircuitComponent<ScalarT, IdxT>::tag_;
+    using CircuitComponent<ScalarT, IdxT>::f_;
+    using CircuitComponent<ScalarT, IdxT>::g_;
+    using CircuitComponent<ScalarT, IdxT>::yB_;
+    using CircuitComponent<ScalarT, IdxT>::ypB_;
+    using CircuitComponent<ScalarT, IdxT>::fB_;
+    using CircuitComponent<ScalarT, IdxT>::gB_;
+    using CircuitComponent<ScalarT, IdxT>::jac_;
+    using CircuitComponent<ScalarT, IdxT>::param_;
+    using CircuitComponent<ScalarT, IdxT>::idc_;
+
+    using CircuitComponent<ScalarT, IdxT>::extern_indices_;
+    using CircuitComponent<ScalarT, IdxT>::n_extern_;
+    using CircuitComponent<ScalarT, IdxT>::n_intern_;
+
+  public:
+    HiresBus(IdxT id)
+    {
+      size_           = 2;
+      n_intern_       = 2;
+      n_extern_       = 0;
+      extern_indices_ = {};
+      idc_            = id;
+    }
+
+    ~HiresBus()
+    {
+    }
+
+    int allocate()
+    {
+      y_.resize(static_cast<size_t>(size_));
+      yp_.resize(static_cast<size_t>(size_));
+      f_.resize(static_cast<size_t>(size_));
+
+      return 0;
+    }
+
+    int initialize()
+    {
+      return 0;
+    }
+
+    int tagDifferentiable()
+    {
+      return 0;
+    }
+
+    int evaluateResidual()
+    {
+      f_[0] = -y_[0];
+      f_[1] = -y_[1];
+
+      return 0;
+    }
+
+    int evaluateJacobian()
+    {
+      return 0;
+    }
+
+    int evaluateIntegrand()
+    {
+      return 0;
+    }
+
+    int initializeAdjoint()
+    {
+      return 0;
+    }
+
+    int evaluateAdjointResidual()
+    {
+      return 0;
+    }
+
+    int evaluateAdjointIntegrand()
+    {
+      return 0;
+    }
+  };
+} // namespace GridKit
