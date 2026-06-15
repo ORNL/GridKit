ae43b3f4d3b37cc62828de4e7785e88db206d8c0
abdourahmanbarry <abdourahman@vt.edu>
2026-06-15 01:47:12 +0000

    Work in progress cosimulation implementation: in rought shape

--- GridKit/Solver/Dynamic/MultiStageCosimulation.cpp
+++ GridKit/Solver/Dynamic/MultiStageCosimulation.cpp
@@ -5,52 +5,99 @@
 
 #include <cmath>
 #include <cstddef>
+#include <memory>
 #include <vector>
+
+#include "GridKit/Solver/Dynamic/Interpolation.hpp"
+#include "GridKit/Solver/Dynamic/Rosenbrock.hpp"
 
 namespace Integrator
 {
+
+  template <class ScalarT, typename IdxT>
+  MultiStageCosimulation<ScalarT, IdxT>::MultiStageCosimulation(
+      std::vector<PartitionEvaluator*> partitions,
+      std::size_t                      num_stages,
+      MemorySpace                      memspace)
+    : num_partitions_(partitions.size()),
+      num_stages_(num_stages),
+      memspace_(memspace),
+      partitions_(std::move(partitions))
+  {
+    if (num_partitions_ == 0)
+    {
+      throw std::invalid_argument("MultiStageCosimulation requires at least one partition.");
+    }
+
+    if (num_stages_ == 0)
+    {
+      throw std::invalid_argument("MultiStageCosimulation requires at least one stage.");
+    }
+
+    for (auto* partition : partitions_)
+    {
+      if (partition == nullptr)
+      {
+        throw std::invalid_argument("Partition pointer cannot be null.");
+      }
+    }
+  }
 
   // TODO: Complete implementation
   template <class ScalarT, typename IdxT>
   int MultiStageCosimulation<ScalarT, IdxT>::allocate()
   {
+    num_partitions_ = partitions_.size();
+
+    x0_local_.resize(num_partitions_);
+    coupling_mat_.resize(num_partitions_);
+    linear_workspaces_.resize(num_partitions_);
+    lin_solver_.resize(num_partitions_);
+    solvers_.resize(num_partitions_);
 
     for (size_t p = 0; p < num_partitions_; ++p)
     {
       auto* part = partitions_[p];
+      assert(part != nullptr);
 
-      int num_coupling = part->getCouplingSize();
-      int num_states   = part->getStateSize();
+      const IdxT num_states   = part->getStateSize();
+      const IdxT num_coupling = part->getCouplingSize();
 
-      x0_local_[p]     = State(num_states);
-      coupling_mat_[p] = State(num_coupling, num_stages_);
+      x0_local_[p] = std::make_unique<State>(num_states);
+      x0_local_[p]->allocate(memspace_);
 
-      x0_local_[p].allocate(memspace_);
-      coupling_mat_[p].allocate(memspace_);
+      coupling_mat_[p] = std::make_unique<State>(num_coupling);
+      coupling_mat_[p]->allocate(memspace_);
+
+      linear_workspaces_[p] = std::make_unique<ReSolve::LinAlgWorkspaceCpu>();
+
+      lin_solver_[p] = std::make_unique<ReSolve::SystemSolver>(
+          linear_workspaces_[p].get(),
+          "klu",
+          "klu",
+          "klu");
+
+      lin_solver_[p]->initialize();
+
+      solvers_[p] = std::make_unique<Rosenbrock>(
+          Rosenbrock::Tableau::rodas5p(),
+          part,
+          lin_solver_[p].get(),
+          &vector_handler_);
+
+      solvers_[p]->allocate();
     }
 
     return 0;
   }
 
   template <class ScalarT, typename IdxT>
-  int MultiStageCosimulation<ScalarT, IdxT>::distributeLocal(const State& global_y)
+  int MultiStageCosimulation<ScalarT, IdxT>::distributeLocal(const State& global_y, PartitionEvaluator* partition)
   {
+    auto&  internal = partition->getInternalIndecies();
+    size_t n        = partition->size();
 
-    auto* global_data = global_y.getData(memspace_);
-
-    for (size_t p = 0; p < num_partitions_; ++p)
-    {
-      auto*  part     = partitions_[p];
-      auto&  internal = part->getInternalIndecies(); // TODO: add this function to partition evaluate
-      size_t n        = part->size();
-
-      auto* local_data = x0_local_[p].getData(memspace_);
-
-      for (size_t i = 0; i < n; ++i)
-      {
-        local_data[i] = global_data[internal[i]];
-      }
-    }
+    matrix_handler_.matvec();
 
     return 0;
   }
@@ -87,32 +134,53 @@ namespace Integrator
 
   // TODO: Complete implementation
   template <class ScalarT, typename IdxT>
-  int MultiStageCosimulation<ScalarT, IdxT>::partitionSolve()
+  int MultiStageCosimulation<ScalarT, IdxT>::partitionStageSolve(int stage, double t0, double dt)
   {
 
-    std::vector<double> output_time = output_times;
+    std::vector<double> out_times;
 
-    for (size_t i = 0; i < max_iterations_; i++)
+    for (size_t iter = 0; iter < max_iterations_; iter++)
     {
-      for (auto* partitionEvaluator : partitions_)
+      for (size_t part = 0; part < num_partitions_; part++)
       {
 
-        // TODO: add a coupling function to the partition
-        partitionEvaluator->addForcing();
+        auto* partitionEvaluator = partitions_[part];
+        auto* partition_solver   = solvers_[part];
 
-        if (i + 1 < max_iterations_)
+        if (stage == 0 && iter == 0)
         {
-          solver.integrate({t0}, partition_solution_.getData(stage));
+          distributeLocal(y_, partitionEvaluator);
         }
-        else
+
+        auto& coupling_stage_values = coupling_mat_[part];
+
+        GridKit::LagrangeInterpolant interp(coupling_stage_values, t0, dt);
+        partitionEvaluator->addForcing(interp);
+
+        for (size_t i = 0; i < num_partitions_; i++)
         {
-          solver.integrate(output_time, partition_solution_);
+          partition_solution_[i]->setToZero(memspace_);
         }
+
+        int counter = 1;
+
+        auto sol_out = [&](double tt, ReSolve::vector::Vector yp) -> void
+        {
+          auto* internal_csr = partition_internal_csr_[part].get();
+          auto* internal_sol = partition_solution_[counter].get();
+
+          double alpha = 1.0;
+          double beta  = 1.0;
+
+          matrix_handler_.matvec(internal_csr, &yp, internal_sol, &alpha, &beta, memspace_);
+
+          counter++;
+        };
+
+        partition_solver->integrate(out_times, stepController_, params_, sol_out);
       }
 
-      updateCoupling(stage + 1)
-
-          if (stage == 0)
+      if (stage == 0)
       {
         break;
       }
