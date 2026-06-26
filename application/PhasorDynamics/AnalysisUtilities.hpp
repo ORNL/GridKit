c16cd5c70a9f8dbc43616470f26bf616af8a7562
Philip Fackler <facklerpw@ornl.gov>
2026-06-26 12:00:46 -0500

    Moved explicit tiny cases to integration tests and standardized examples

--- application/PhasorDynamics/AnalysisUtilities.hpp
+++ application/PhasorDynamics/AnalysisUtilities.hpp
@@ -67,7 +67,7 @@ namespace GridKit
       /// path to reference file for validation
       fs::path                 reference_file;
       /// Error tolerance (between output file and reference file)
-      double                   error_tol;
+      std::vector<double>      error_tol;
       /// Type of total error (relative or absolute)
       Testing::ErrorType       error_type;
       /// Smallest value at which to scale for relative error
@@ -122,7 +122,23 @@ namespace GridKit
         j.at("reference_file").get_to(c.reference_file);
       }
 
-      c.error_tol = j.value("error_tolerance", 1.0e-4);
+      if (j.contains("error_tolerance"))
+      {
+        auto& tolj = j.at("error_tolerance");
+        if (tolj.is_array())
+        {
+          tolj.get_to(c.error_tol);
+        }
+        else
+        {
+          tolj.get_to(c.error_tol.emplace_back());
+          // c.error_tol.push_back(j.value("error_tolerance", 1.0e-4));
+        }
+      }
+      else
+      {
+        c.error_tol.push_back(1.0e-4);
+      }
 
       using ErrorType = Testing::ErrorType;
       if (j.contains("error_type"))
@@ -270,7 +286,18 @@ namespace GridKit
         }
 
         // Check against specified tolerance
-        status *= errorSet->total_error.max_value < study_data.error_tol;
+        if (study_data.error_tol.size() > 1)
+        {
+          status *= study_data.error_tol.size() == errorSet->var_errors.size();
+          for (std::size_t i = 0; i < study_data.error_tol.size(); ++i)
+          {
+            status *= errorSet->var_errors[i].max_value < study_data.error_tol[i];
+          }
+        }
+        else
+        {
+          status *= errorSet->total_error.max_value < study_data.error_tol[0];
+        }
 
         if (print_results)
         {
