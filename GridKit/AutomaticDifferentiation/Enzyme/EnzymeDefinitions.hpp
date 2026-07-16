a209dd8ae18893fd9edba6492b74e5d0e9d00e0b
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2026-07-16 13:11:08 -0400

    Scalar Enzyme test as unit test.

--- GridKit/AutomaticDifferentiation/Enzyme/EnzymeDefinitions.hpp
+++ GridKit/AutomaticDifferentiation/Enzyme/EnzymeDefinitions.hpp
@@ -23,20 +23,31 @@ namespace GridKit
 {
   namespace Enzyme
   {
+    namespace Scalar
+    {
+      /**
+       * @brief Enzyme autodiff template for scalar functions
+       *
+       * @tparam scalar_type - scalar type
+       */
+      template <typename scalar_type>
+      extern scalar_type __enzyme_autodiff(scalar_type (*)(scalar_type), ...);
+    } // namespace Scalar
+
     namespace Sparse
     {
       /**
-       * @brief Enzyme fwddiff template
+       * @brief Enzyme fwddiff template for GridKit models
        *
        * @details This is a core templated intrinsic that the Enzyme pass will
        * use to perform automatic differenciation. We define the template here
        * so it can later be used in different places.
        *
        * @tparam T - return type
-       * @tparam ModelT - model type
+       * @tparam model_type - model type
        */
-      template <typename T, typename... ModelT>
-      extern T __enzyme_fwddiff(void*, ModelT...) noexcept;
+      template <typename T, typename... model_type>
+      extern T __enzyme_fwddiff(void*, model_type...) noexcept;
     } // namespace Sparse
   } // namespace Enzyme
 } // namespace GridKit
--- tests/UnitTests/AutomaticDifferentiation/Enzyme/EnzymeTests.hpp
+++ tests/UnitTests/AutomaticDifferentiation/Enzyme/EnzymeTests.hpp
@@ -25,9 +25,34 @@ namespace GridKit
       TestOutcome scalar_square()
       {
         TestStatus success = true;
+  
+        ScalarT var        = 5.0;
+        ScalarT sq         = square(var);
+        ScalarT dsq_ref    = dsquare(var);
+        ScalarT dsq_enzyme = GridKit::Enzyme::Scalar::__enzyme_autodiff<ScalarT>(square, var);
 
+        success *= GridKit::Testing::isEqual(dsq_enzyme, dsq_ref);
+
+        if (!success)
+        {
+          std::cout << "x = " << var << "\n"
+                    << "x^2 = " << sq << "\n"
+                    << "Reference d(x^2)/dx = " << dsq_ref << "\n"
+                    << "Enzyme d(x^2)/dx = " << dsq_enzyme << "\n";
+        }
 
         return success.report(__func__);
+      }
+
+    private:
+      static ScalarT square(ScalarT x)
+      {
+        return x * x;
+      }
+
+      static ScalarT dsquare(ScalarT x)
+      {
+        return 2.0 * x;
       }
     };
 
