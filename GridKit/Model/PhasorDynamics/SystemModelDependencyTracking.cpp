98debc52b81d0e1074eb4294bba8369f74fb85dd
Nicholson Koukpaizan <72402802+nkoukpaizan@users.noreply.github.com>
2026-07-22 09:06:17 -0400

    Message about expected errors in tests (#500)
    * Add commas to DependencyTracking::Variable::print.
    
    * Split SystemModel compilation units with and without Enzyme... Suppresses some warnings.
    
    * Message about expected errors in SystemTests.
    
    * Message about expected errors in Branch and Exciter unit tests.
    
    * Missing expected error messages in CliArgsTests.
    
    * std::cout --> Log::misc() for expected errors messages.
    
    * Message about expected errors for VectorTest and ensure verbosity is reset to WARNINGS.

--- GridKit/Model/PhasorDynamics/SystemModelDependencyTracking.cpp
+++ GridKit/Model/PhasorDynamics/SystemModelDependencyTracking.cpp
@@ -4,6 +4,18 @@ namespace GridKit
 {
   namespace PhasorDynamics
   {
+    /**
+     * @brief By default, Jacobians are not available
+     *
+     * DependencyTracking::Variable stores the Jacobian as dependency maps.
+     * @todo Construct a Jacobian based on the dependency maps.
+     */
+    template <typename scalar_type, typename index_type>
+    bool SystemModel<scalar_type, index_type>::hasJacobian()
+    {
+      return false;
+    }
+
     // Available template instantiations
     // template class SystemModel<DependencyTracking::Variable, long int>;
     template class SystemModel<DependencyTracking::Variable, size_t>;
