e91db8e20d69a66bc81d19584659bcba6532e8be
lukelowry <wyattluke.lowery@gmail.com>
2026-07-27 21:12:45 -0500

    PERFORMANCE: further eliminate genrou

--- GridKit/Model/PhasorDynamics/SynchronousMachine/GENROU/GenrouImpl.hpp
+++ GridKit/Model/PhasorDynamics/SynchronousMachine/GENROU/GenrouImpl.hpp
@@ -49,7 +49,7 @@ namespace GridKit
         S12_(0.),
         mva_base_(100.)
     {
-      size_ = 17;
+      size_ = 15;
       setDerivedParams();
     }
 
@@ -98,7 +98,7 @@ namespace GridKit
         S12_(S12),
         mva_base_(100.)
     {
-      size_ = 17;
+      size_ = 15;
       setDerivedParams();
     }
 
@@ -114,7 +114,7 @@ namespace GridKit
       initializeParameters(data);
       initializeMonitor();
 
-      size_ = 17;
+      size_ = 15;
       setDerivedParams();
     }
 
@@ -131,7 +131,7 @@ namespace GridKit
       initializeParameters(data);
       initializeMonitor();
 
-      size_ = 17;
+      size_ = 15;
       setDerivedParams();
     }
 
@@ -149,7 +149,7 @@ namespace GridKit
       initializeParameters(data);
       initializeMonitor();
 
-      size_ = 17;
+      size_ = 15;
       setDerivedParams();
     }
 
@@ -278,13 +278,13 @@ namespace GridKit
       using Variable = typename ModelDataT::MonitorableVariables;
       // Convert monitored terminal values to system base.
       monitor_->set(Variable::ir, [this]
-                    { return this->toSystemBase(y_.getData()[15]); });
+                    { return this->toSystemBase(y_.getData()[13]); });
       monitor_->set(Variable::ii, [this]
-                    { return this->toSystemBase(y_.getData()[16]); });
+                    { return this->toSystemBase(y_.getData()[14]); });
       monitor_->set(Variable::p, [this]
-                    { return this->toSystemBase(Vr() * y_.getData()[15] + Vi() * y_.getData()[16]); });
+                    { return this->toSystemBase(Vr() * y_.getData()[13] + Vi() * y_.getData()[14]); });
       monitor_->set(Variable::q, [this]
-                    { return this->toSystemBase(Vi() * y_.getData()[15] - Vr() * y_.getData()[16]); });
+                    { return this->toSystemBase(Vi() * y_.getData()[13] - Vr() * y_.getData()[14]); });
       monitor_->set(Variable::delta, [this]
                     { return y_.getData()[0]; });
       monitor_->set(Variable::omega, [this]
@@ -422,21 +422,17 @@ namespace GridKit
       y[7]           = psidpp;
       y[8] = psipp = std::sqrt(psiqpp * psiqpp + psidpp * psidpp);
       y[9] = ksat = SB_ * Math::qramp(psipp - SA_);
-      ScalarT vd  = -psiqpp * (ONE<RealT> + omega);
-      vq          = psidpp * (ONE<RealT> + omega);
-      y[10]       = vd;
-      y[11]       = vq;
-      y[12]       = (psidpp - id * Xdpp_) * iq - (psiqpp - iq * Xdpp_) * id;
-      y[13]       = id;
-      y[14]       = iq;
-      y[15]       = ir;
-      y[16]       = ii;
+      y[10]       = (psidpp - id * Xdpp_) * iq - (psiqpp - iq * Xdpp_) * id;
+      y[11]       = id;
+      y[12]       = iq;
+      y[13]       = ir;
+      y[14]       = ii;
 
       // TODO: Writing to an input signal seems very wrong. Check to see if this
       //       could be handled differently
 
       // Convert Te to system base for governor PM signal.
-      ScalarT Te = y[12];
+      ScalarT Te = y[10];
       pmech_set_ = static_cast<RealT>(this->toSystemBase(Te));
       if (auto pmech_port = ports_.in.template port<GenrouSignalInputs::pmech>())
       {
@@ -521,13 +517,11 @@ namespace GridKit
       ScalarT psidpp = y[7];
       ScalarT psipp  = y[8];
       ScalarT ksat   = y[9];
-      ScalarT vd     = y[10];
-      ScalarT vq     = y[11];
-      ScalarT telec  = y[12];
-      ScalarT id     = y[13];
-      ScalarT iq     = y[14];
-      ScalarT ir     = y[15];
-      ScalarT ii     = y[16];
+      ScalarT telec  = y[10];
+      ScalarT id     = y[11];
+      ScalarT iq     = y[12];
+      ScalarT ir     = y[13];
+      ScalarT ii     = y[14];
 
       /* Read derivatives */
       ScalarT delta_dot = yp[0];
@@ -552,8 +546,8 @@ namespace GridKit
       const ScalarT cos_delta = std::cos(delta);
 
       // Internal Voltage
-      const ScalarT Vint_r = sin_delta * vd + cos_delta * vq;
-      const ScalarT Vint_i = -cos_delta * vd + sin_delta * vq;
+      const ScalarT Vint_r = (-sin_delta * psiqpp + cos_delta * psidpp) * (ONE<RealT> + omega);
+      const ScalarT Vint_i = (cos_delta * psiqpp + sin_delta * psidpp) * (ONE<RealT> + omega);
 
       /* 6 Genrou differential equations */
       f[0] = delta_dot - omega * (TWO<RealT> * pi * freq_system_base_);
@@ -563,20 +557,18 @@ namespace GridKit
       f[4] = psiqp_dot - (ONE<RealT> / Tqopp_) * (Edp - psiqp + Xq2_ * iq);
       f[5] = Edp_dot - (ONE<RealT> / Tqop_) * (-Edp + Xqd_ * psiqpp * ksat + Xq1_ * (iq - Xq3_ * (Edp + iq * Xq2_ - psiqp)));
 
-      /* 9 Genrou algebraic equations */
+      /* 7 Genrou algebraic equations */
       f[6]  = psiqpp - (-psiqp * Xq4_ - Edp * Xq5_);
       f[7]  = psidpp - (psidp * Xd4_ + Eqp * Xd5_);
       f[8]  = psipp - std::sqrt((psidpp * psidpp) + (psiqpp * psiqpp));
       f[9]  = ksat - SB_ * Math::qramp(psipp - SA_);
-      f[10] = vd + psiqpp * (ONE<RealT> + omega);
-      f[11] = vq - psidpp * (ONE<RealT> + omega);
-      f[12] = telec - ((psidpp - id * Xdpp_) * iq - (psiqpp - iq * Xdpp_) * id);
-      f[13] = id - (ir * sin_delta - ii * cos_delta);
-      f[14] = iq - (ir * cos_delta + ii * sin_delta);
+      f[10] = telec - ((psidpp - id * Xdpp_) * iq - (psiqpp - iq * Xdpp_) * id);
+      f[11] = id - (ir * sin_delta - ii * cos_delta);
+      f[12] = iq - (ir * cos_delta + ii * sin_delta);
 
       /* 2 Genrou network equations */
-      f[15] = ir + G_ * vr - B_ * vi - (G_ * Vint_r - B_ * Vint_i);
-      f[16] = ii + B_ * vr + G_ * vi - (B_ * Vint_r + G_ * Vint_i);
+      f[13] = ir + G_ * vr - B_ * vi - (G_ * Vint_r - B_ * Vint_i);
+      f[14] = ii + B_ * vr + G_ * vi - (B_ * Vint_r + G_ * Vint_i);
 
       return 0;
     }
@@ -592,8 +584,8 @@ namespace GridKit
         [[maybe_unused]] const ScalarT* wb,
         ScalarT*                        h)
     {
-      ScalarT ir = y[15];
-      ScalarT ii = y[16];
+      ScalarT ir = y[13];
+      ScalarT ii = y[14];
 
       // Convert current injection to system base for the network.
       h[0] = this->toSystemBase(ir);
