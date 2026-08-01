3aebe66c582ec9711dae4ab55895f986058edd8b
lukelowry <wyattluke.lowery@gmail.com>
2026-07-31 20:35:42 -0500

    remove compatability parameter Vnom

--- GridKit/Model/PhasorDynamics/Load/LoadZIP/LoadZIPData.hpp
+++ GridKit/Model/PhasorDynamics/Load/LoadZIP/LoadZIPData.hpp
@@ -12,7 +12,6 @@ namespace GridKit
     {
       Pnom,   ///< Nominal real power
       Qnom,   ///< Nominal reactive power
-      Vnom,   ///< Nominal voltage magnitude
       alphaI, ///< Fraction of load represented as constant current
       alphaP, ///< Fraction of load represented as constant power
     };
