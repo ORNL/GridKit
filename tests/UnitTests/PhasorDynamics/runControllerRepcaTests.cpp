ce05cd679ee363b426d7afccbb4e322deae3e0fc
lukelowry <wyattluke.lowery@gmail.com>
2026-08-24 11:58:47 -0500

    logger output

--- tests/UnitTests/PhasorDynamics/runControllerRepcaTests.cpp
+++ tests/UnitTests/PhasorDynamics/runControllerRepcaTests.cpp
@@ -1,7 +1,11 @@
+#include <GridKit/Utilities/Logger/Logger.hpp>
+
 #include "ControllerRepcaTests.hpp"
 
 int main()
 {
+  GridKit::Utilities::Logger::setVerbosity(GridKit::Utilities::Logger::Verbosity::NONE);
+
   GridKit::Testing::TestingResults result;
 
   GridKit::Testing::ControllerRepcaTests<double, size_t> test;
--- tests/UnitTests/PhasorDynamics/runSystemSingleComponentTests.cpp
+++ tests/UnitTests/PhasorDynamics/runSystemSingleComponentTests.cpp
@@ -1,7 +1,11 @@
+#include <GridKit/Utilities/Logger/Logger.hpp>
+
 #include "SystemSingleComponentTests.hpp"
 
 int main()
 {
+  GridKit::Utilities::Logger::setVerbosity(GridKit::Utilities::Logger::Verbosity::NONE);
+
   using namespace GridKit;
   using namespace GridKit::Testing;
 
