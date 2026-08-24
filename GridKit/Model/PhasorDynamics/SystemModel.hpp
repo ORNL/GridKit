8265c5941f12a2bbf0ded93813f1306c4afdc492
lukelowry <wyattluke.lowery@gmail.com>
2026-08-24 14:47:28 -0500

    System external/residual calls

--- GridKit/Model/PhasorDynamics/SystemModel.hpp
+++ GridKit/Model/PhasorDynamics/SystemModel.hpp
@@ -92,6 +92,8 @@ namespace GridKit
 
       int tagDifferentiable() override;
       int setAbsoluteTolerance(RealT rel_tol) override;
+      int evaluateInternalResidual() override;
+      int evaluateExternalResidual() override;
       int evaluateResidual() override;
       int evaluateJacobian() override;
 
