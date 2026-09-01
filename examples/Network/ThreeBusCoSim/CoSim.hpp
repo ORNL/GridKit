382aa63bfc52929cf13a71ed6a07ceb690de9f06
Philip Fackler <facklerpw@ornl.gov>
2026-09-01 11:46:19 -0500

    Add comments about status keywords

--- examples/Network/ThreeBusCoSim/CoSim.hpp
+++ examples/Network/ThreeBusCoSim/CoSim.hpp
@@ -4,6 +4,17 @@ namespace GridKit
 {
   namespace CoSim
   {
+    /*
+     * The following is meant to be used to communicate driver information over
+     * the network. It is crude, but it at least gives language in the code that
+     * expresses the meaning of the intent.
+     *
+     * - STEP is used to initiate (and continue) the co-simulation (take a step)
+     * - END is used to end the co-simulation
+     *
+     * Both of these are sent by the client to keep the server in step.
+     */
+
     using StatusT = int;
 
     inline constexpr StatusT STEP = 1;
