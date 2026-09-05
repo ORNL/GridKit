ecd756d4df9b28785b716de4cb4c4770cbff8115
Nicholson Koukpaizan <koukpaizannk@ornl.gov>
2026-09-05 19:17:01 -0400

    Set variableNumber in SystemModel's initialize().

--- GridKit/Model/PhasorDynamics/SystemModelDependencyTracking.cpp
+++ GridKit/Model/PhasorDynamics/SystemModelDependencyTracking.cpp
@@ -29,7 +29,7 @@ namespace GridKit
       using DependencyMap = typename ScalarT::DependencyMap;
     
       const auto* f = f_.getData();
-    
+
       if (csr_jac_ == nullptr)
       {
         IdxT* row_ptrs = new IdxT[static_cast<size_t>(size_) + 1];
@@ -47,13 +47,13 @@ namespace GridKit
     
             // Merge-count y and yp dependencies
             IdxT jac_col;
-            if (col < size_)
+            if (col < y_yp_offset_)
             {
               jac_col = col;
             }
             else
             {
-              jac_col = col - size_;
+              jac_col = col - y_yp_offset_;
             }
     
             if (row_map.insert({jac_col, RealT{}}).second)
@@ -80,14 +80,14 @@ namespace GridKit
             const IdxT col = static_cast<IdxT>(dep.first);
     
             IdxT jac_col;
-            if (col < size_)
+            if (col < y_yp_offset_)
             {
               jac_col = col;
               row_map[jac_col] += static_cast<RealT>(dep.second);
             }
             else
             {
-              jac_col = col - size_;
+              jac_col = col - y_yp_offset_;
               row_map[jac_col] += alpha_ * static_cast<RealT>(dep.second);
             }
           }
@@ -117,14 +117,14 @@ namespace GridKit
             const IdxT col = static_cast<IdxT>(dep.first);
     
             IdxT jac_col;
-            if (col < size_)
+            if (col < y_yp_offset_)
             {
               jac_col = col;
               row_map[jac_col] += static_cast<RealT>(dep.second);
             }
             else
             {
-              jac_col = col - size_;
+              jac_col = col - y_yp_offset_;
               row_map[jac_col] += alpha_ * static_cast<RealT>(dep.second);
             }
           }
