ba6e2f271a7630f71f98b59de457737bcf2a2c4a
nkoukpaizan <nkoukpaizan@users.noreply.github.com>
2026-09-10 20:21:17 +0000

    Apply pre-commit fixes

--- examples/PowerElectronics/ExamplesHelper/SystemAssembler.hpp
+++ examples/PowerElectronics/ExamplesHelper/SystemAssembler.hpp
@@ -32,9 +32,9 @@
  *       does not call PowerElectronics::SystemModel::allocate().
  */
 template <class ScalarT, typename IdxT>
-void assembleSystem(ScaleMicrogridNetwork<ScalarT, IdxT>& network, 
-                    GridKit::PowerElectronics::SystemModel<ScalarT, 
-                    IdxT>& sys_model)
+void assembleSystem(ScaleMicrogridNetwork<ScalarT, IdxT>&         network,
+                    GridKit::PowerElectronics::SystemModel<ScalarT,
+                                                           IdxT>& sys_model)
 {
   size_t N_size = network.N_size;
 
